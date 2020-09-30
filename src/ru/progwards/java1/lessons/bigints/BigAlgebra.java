77a3d667165960c0f1338c17b1bfcf9f4ef0f03f
Twist <evgen-shevel@yandex.ru>
2020-09-30 16:11:12 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
+++ src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
@@ -8,33 +8,35 @@ public class BigAlgebra {
     public BigAlgebra() {
     }
 
-    BigDecimal fastPow(BigDecimal num, int pow) {
-        return null;
-    }
+     BigDecimal fastPow(BigDecimal num, int pow) {
 
-    public static void main(String[] args) {
-
-        BigDecimal num = new BigDecimal("928372.333");
-
-        int n = 5;
-
-        BigDecimal result = num.pow(n);
-
-        System.out.println(result);
-
-    }
-
-    BigInteger fibonacci(int n) {
-
-        if (n <= 2) return BigInteger.ONE;
-
-        BigInteger prev = BigInteger.ONE, sum = BigInteger.ONE;
-        BigInteger next = null;
-        for (int i = 2; i < n; i++) {
-            next = prev.add(sum);
-            sum = prev;
-            prev = next;
+        if (pow == 0) return BigDecimal.valueOf(1.);        //проверка нулевой степени
+        String BYNPOW = Integer.toString(pow, 2);           //представление степени двоичным числом
+        BigDecimal result = BigDecimal.valueOf(0);             //инициализация результата НУЛЕМ
+        for (int i = BYNPOW.length() - 1; i <= 0; i++) {    //цикл перебора от СТАРШЕГО бита к МЛАДШЕМУ
+            if (BYNPOW.charAt(i) == '1') {                           //TODO
+                result = result.multiply(result).multiply(num);
+            } else result = result.multiply(result);
         }
-        return next;
+        return result;
+    }
+
+    //public static void main(String[] args) {
+
+       // System.out.println(fastPow(BigDecimal.valueOf(3), 5));
+       // System.out.println(new BigInteger(String.valueOf(fibonacci(10000))));
+    //}
+
+     BigInteger fibonacci(int n) {
+        if (n <= 2) return BigInteger.ONE;
+        BigInteger prev = BigInteger.ONE, prevOfPrev = BigInteger.ONE;
+        BigInteger curr = null;
+        for (int i = 2; i < n; i++) {
+            curr = prev.add(prevOfPrev);
+            prevOfPrev = prev;
+            prev = curr;
+        }
+        return curr;
+
     }
 }
