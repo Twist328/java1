925d5d7ed0a0adb5190a7f3afff5e832251edded
Twist <evgen-shevel@yandex.ru>
2020-10-01 14:53:38 +0300

    AVK

---
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -0,0 +1,16 @@
+package ru.progwards.java1.lessons.bigints;
+
+ class AbsInteger {
+     public AbsInteger() {
+     }
+ }
+
+    class ByteInteger extends AbsInteger {
+
+        }
+
+      class   ShortInteger extends AbsInteger {
+
+    }
+    class IntInteger extends AbsInteger {
+}
