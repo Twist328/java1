fc9ffa4972a588da1087e02162194fbbe2a9acb5
Twist <evgen-shevel@yandex.ru>
2020-10-01 14:16:14 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
+++ src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
@@ -13,23 +13,23 @@ public class BigAlgebra {
          return num.pow(pow);
     }
     public static void main(String[] args) {
-        BigAlgebra cadabra = new BigAlgebra();
-        System.out.println(cadabra.fastPow(BigDecimal.valueOf(8), 5));
+        BigAlgebra dec = new BigAlgebra();
+        System.out.println(dec.fastPow(BigDecimal.valueOf(256.33), 5));
 
 
-        System.out.println(new BigInteger(String.valueOf(fibonacci(15))));
+        System.out.println(new BigInteger(String.valueOf(fibonacci(66999))));
     }
 
      static BigInteger fibonacci(int n) {
         if (n <= 2) return BigInteger.valueOf(1);
-        BigInteger prev = BigInteger.valueOf(1), prevOfPrev = BigInteger.valueOf(1);
-        BigInteger curr = null;
+        BigInteger prev = BigInteger.valueOf(1), sum = BigInteger.valueOf(1);
+        BigInteger next = null;
         for (int i = 2; i < n; i++) {
-            curr = prev.add(prevOfPrev);
-            prevOfPrev = prev;
-            prev = curr;
+            next = prev.add(sum);
+            sum = prev;
+            prev = next;
         }
-        return curr;
+        return next;
 
     }
 }
