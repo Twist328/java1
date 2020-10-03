7aa4093c7fba4ecdc2cb1f6971d45744e17fb980
Twist328 <evgen-shevel@yandex.ru>
2020-10-03 18:08:48 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -13,6 +13,11 @@ public class ArrayInteger {
     private final static BigDecimal BD_TEN = BigDecimal.valueOf(10);
     private Byte[] digits;
 
+    public static void main(String[] args) {
+        ArrayInteger num = new ArrayInteger(3);
+        System.out.println(num);
+    }
+
     public ArrayInteger( int n) {
         digits = new Byte[n];
     }
@@ -23,7 +28,7 @@ public class ArrayInteger {
             list.add(value.mod(BI_TEN).byteValueExact());
             value = value.divide(BI_TEN);
         }
-        Collections.reverse(list);
+        //Collections.reverse(list);
         digits = list.toArray(new Byte[list.size()]);
     }
 
@@ -40,18 +45,20 @@ public class ArrayInteger {
         int ten = 0;
         final List<Byte> list = new ArrayList<>();
         for (int i = Math.max(num.digits.length, this.digits.length) - 1; i >= 0; i--) {
-            int result = ((num.digits.length <= i) ? 0 : num.digits[i]) + ((this.digits.length <= i) ? 0 : this.digits[i]) + ten;
+            int result = ((num.digits.length <= i) ? 0 : num.digits[i]) +
+                    ((this.digits.length <= i) ? 0 : this.digits[i]) + ten;
             ten = result > 9 ? 1 : 0;
             list.add((byte) (result % 10));
         }
-        Collections.reverse(list);
+        //Collections.reverse(list);
         digits = list.toArray(new Byte[list.size()]);
         return true;
     }
 
-    @Override
+   @Override
     public String toString() {
-        return digits == null ? null : Arrays.toString(digits);
+       return digits == null ? null : Arrays.toString(digits);
     }
 
 }
+
