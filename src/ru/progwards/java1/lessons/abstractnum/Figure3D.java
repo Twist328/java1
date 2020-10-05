f32bb403986452739ada076752a48756d268799e
Twist328 <evgen-shevel@yandex.ru>
2020-10-05 20:08:28 +0300

    AVK

---
+++ src/ru/progwards/java1/lessons/abstractnum/Figure3D.java
@@ -0,0 +1,14 @@
+package ru.progwards.java1.lessons.abstractnum;
+
+public class Figure3D {
+    Number segment;
+
+    public Figure3D(Number psegment) {
+        segment = psegment;
+    }
+
+    public Number volume() {
+        this.volume();
+        return null;
+    }
+}
