919432a9f397ab2597ddfda929593c818350157d
Twist <evgen-shevel@yandex.ru>
2020-10-08 13:11:01 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -12,12 +12,12 @@ public class AbsInteger {
         return 0;
     }
 
-    static AbsInteger add(AbsInteger num1, AbsInteger num2) {
+    static AbsInteger add(AbsInteger num1,AbsInteger num2) {
         int a = num1.intValue();
         int b = num2.intValue();
         byte num= (byte) (a+b);
 
-        return new AbsInteger((byte) num);
+        return new ByteInteger((byte) num);
 
     }
 
@@ -84,12 +84,12 @@ static class IntInteger extends AbsInteger {
 }
     public static void main(String[] args) {
 
-        AbsInteger num = new AbsInteger((byte) 6);
+        AbsInteger absInteger = new  ByteInteger((byte) 9);
         IntInteger num5 = new IntInteger(9);
         ShortInteger num3 = new ShortInteger((short) 8);
         ByteInteger num4 = new ByteInteger((byte) 6);
 
-        System.out.println(num);
+        System.out.println(absInteger);
         System.out.println(num3);
         System.out.println(num4);
         System.out.println(num5);
