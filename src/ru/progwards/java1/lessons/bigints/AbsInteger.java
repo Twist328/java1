94575d9c0f9cad236d6078598fe3717c59a66012
Twist <evgen-shevel@yandex.ru>
2020-10-08 12:53:12 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -4,28 +4,28 @@ import static java.lang.Integer.*;
 
 public class AbsInteger {
 
-    public int intValue(){
+    public AbsInteger(byte num) {
+
+    }
+
+    public  int intValue(){
         return 0;
     }
 
     static AbsInteger add(AbsInteger num1, AbsInteger num2) {
-        int sum;
-        AbsInteger a = new AbsInteger();
-        AbsInteger b = new AbsInteger();
-        AbsInteger result = a.add(b);
-        return result;
+        int a = num1.intValue();
+        int b = num2.intValue();
+        byte num= (byte) (a+b);
+
+        return new AbsInteger((byte) num);
 
     }
 
-    private AbsInteger add(AbsInteger b) {
-        return null;
-    }
-}
-
-class ByteInteger extends AbsInteger {
+static class ByteInteger extends AbsInteger {
     byte num;
 
     public ByteInteger(byte num) {
+        super(num);
 
         this.num = num;
     }
@@ -39,12 +39,14 @@ class ByteInteger extends AbsInteger {
     public String toString() {
         return "" + num;
     }
+
 }
 
-class ShortInteger extends AbsInteger {
+static class ShortInteger extends AbsInteger {
     short num;
 
     public ShortInteger(short num) {
+        super((byte) num);
 
         this.num = num;
     }
@@ -61,12 +63,13 @@ class ShortInteger extends AbsInteger {
     }
 }
 
-class IntInteger extends AbsInteger {
+static class IntInteger extends AbsInteger {
     int num;
 
     public IntInteger(int num) {
-
+        super((byte) num);
         this.num = num;
+
     }
 
     @Override
@@ -78,15 +81,17 @@ class IntInteger extends AbsInteger {
     public String toString() {
         return "" + num;
     }
-
+}
     public static void main(String[] args) {
-        AbsInteger res = new AbsInteger();
-        IntInteger num = new IntInteger(9);
+
+        AbsInteger num = new AbsInteger((byte) 6);
+        IntInteger num5 = new IntInteger(9);
         ShortInteger num3 = new ShortInteger((short) 8);
         ByteInteger num4 = new ByteInteger((byte) 6);
-        System.out.println(num3);
+
         System.out.println(num);
+        System.out.println(num3);
         System.out.println(num4);
-        System.out.println(res);
+        System.out.println(num5);
     }
 }
