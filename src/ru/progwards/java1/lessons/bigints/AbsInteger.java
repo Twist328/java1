d2a6949e9045ff5aa34d01fa3f64079411bd1448
Twist328 <evgen-shevel@yandex.ru>
2020-10-08 21:34:07 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -79,11 +79,14 @@ class IntInteger extends AbsInteger {
     public static void main(String[] args) {
 
         AbsInteger absInteger = new ByteInteger((byte) 127);
+        AbsInteger absInteger2 = new IntInteger((int) 127);
         AbsInteger absInteger1= new ShortInteger((short) 889);
         IntInteger num5 = new IntInteger(9);
         ShortInteger num3 = new ShortInteger((short) 8);
         ByteInteger num4 = new ByteInteger((byte) 6);
+
         System.out.println(absInteger1);
+        System.out.println(absInteger2);
         System.out.println(absInteger);
         System.out.println(num3);
         System.out.println(num4);
