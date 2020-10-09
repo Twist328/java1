f57cdc40b497d0590f3a8a95642465d155320306
Twist <evgen-shevel@yandex.ru>
2020-10-09 12:22:47 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -6,20 +6,8 @@ public class AbsInteger {
 
     public static void main(String[] args) {
 
-        //AbsInteger absInteger = new ByteInteger((byte) 127);
-        // AbsInteger absInteger2 = new IntInteger((int) 127);
-        //AbsInteger absInteger1= new ShortInteger((short) 889);
-        // IntInteger num5 = new IntInteger(9);
-        // ShortInteger num3 = new ShortInteger((short) 8);
-        // ByteInteger num4 = new ByteInteger((byte) 6);
         AbsInteger num6 = new ByteInteger((byte) 9);
         AbsInteger num7 = new ByteInteger((byte) 10);
-        //System.out.println(absInteger1);
-        //System.out.println(absInteger2);
-        //System.out.println(absInteger);
-        //System.out.println(num3);
-        //System.out.println(num4);
-        //System.out.println(num5);
 
         int a = num6.intValue();
         int b = num7.intValue();
