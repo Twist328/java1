bdf266218b4b2f835254847b4fe052fc42433fd3
Twist328 <evgen-shevel@yandex.ru>
2020-10-10 01:31:05 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -17,93 +17,38 @@ public class AbsInteger {
         AbsInteger num5 = new ShortInteger((short) 345);
         AbsInteger num6 = new ShortInteger((short) 543);
 
-        int a = num1.intValue();
-        int a1 = num2.intValue();
-        int sum = (a + a1);
-
         int b = num3.byteValue();
         int b1 = num4.byteValue();
         int sum1 = (b + b1);
 
-        int c = num5.shortValue();
-        int c1 = num6.shortValue();
-        int sum2 = (c + c1);
-
-        System.out.println(sum);
         System.out.println(sum1);
-        System.out.println(sum2);
-    }
 
-    public int intValue() {
-        return num;
     }
 
     public byte byteValue() {
         return (byte) num;
     }
 
-    public short shortValue() {
-        return (short) num;
-    }
-
-    static AbsInteger add(AbsInteger num1, AbsInteger num2) {
+        static AbsInteger add(AbsInteger num1, AbsInteger num2) {
         num1 = num1;
         num2 = num2;
+
         int res = 0;
-        int a = num1.intValue();
-        int a1 = num2.intValue();
-        int sum = (a + a1);
+        int a = num1.byteValue();
+        int b = num2.byteValue();
+        int sum = (a + b);
 
-        int b = num1.byteValue();
-        int b1 = num2.byteValue();
-        int sum1 = (b + b1);
-
-        int c = num1.shortValue();
-        int c1 = num2.shortValue();
-        int sum2 = (c + c1);
-
-        int d = num1.intValue();
-        int d1 = num2.byteValue();
-        int sum3 = (d + d1);
-
-        int g = num1.byteValue();
-        int g1 = num2.shortValue();
-        int sum4 = (g + g1);
-
-        switch (res) {
-            case 1:
-                return new IntInteger(sum);
-            case 2:
-                return new ByteInteger((byte) sum1);
-            case 3:
-                return new ShortInteger((short) sum2);
-            case 4:
-                return new IntInteger(sum3);
-            case 5:
-            default:
-                return new ShortInteger((short) sum4);
-
+       return new ByteInteger((byte) sum);
 
         }
-
     }
 
-    static class ByteInteger extends AbsInteger {
+    class ByteInteger extends AbsInteger {
         byte num;
-
-        @Override
-        public int intValue() {
-            return super.intValue();
-        }
 
         @Override
         public byte byteValue() {
             return super.byteValue();
-        }
-
-        @Override
-        public short shortValue() {
-            return super.shortValue();
         }
 
         public ByteInteger(byte num) {
@@ -118,22 +63,12 @@ public class AbsInteger {
         }
     }
 
-    static class ShortInteger extends AbsInteger {
+    class ShortInteger extends AbsInteger {
         short num;
-
-        @Override
-        public int intValue() {
-            return super.intValue();
-        }
 
         @Override
         public byte byteValue() {
             return super.byteValue();
-        }
-
-        @Override
-        public short shortValue() {
-            return super.shortValue();
         }
 
         public ShortInteger(short num) {
@@ -147,22 +82,12 @@ public class AbsInteger {
         }
     }
 
-    static class IntInteger extends AbsInteger {
+    class IntInteger extends AbsInteger {
         int num;
-
-        @Override
-        public int intValue() {
-            return super.intValue();
-        }
 
         @Override
         public byte byteValue() {
             return super.byteValue();
-        }
-
-        @Override
-        public short shortValue() {
-            return super.shortValue();
         }
 
         public IntInteger(int num) {
@@ -175,4 +100,3 @@ public class AbsInteger {
             return "" + num;
         }
     }
-}
