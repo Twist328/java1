0a039a2cc6a7d0f6ae3e846411b5045c9be64ff8
Twist328 <evgen-shevel@yandex.ru>
2020-10-10 20:36:18 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -31,10 +31,10 @@ public class AbsInteger {
         int res = (a + b);
 
         if(res<= -128|res<=127) return new ByteInteger((byte) res);
-        for (int i = res; i > 127 ; i++) {
-            if (res<= -128|res >=-32768|res >127|res <=32767)
+        else//for (int i = res; i > 127 ; i++) {
+            if (res<= -128|res >=-32768|res >127|res <=32767){
             return new ShortInteger((short) res);
-        }
+        }else
         return new IntInteger(res);
 
     }
