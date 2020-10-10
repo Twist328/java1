c791772367c72e15bdb32b6bc2d4a20e4d236b95
Twist328 <evgen-shevel@yandex.ru>
2020-10-10 12:21:53 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -6,93 +6,111 @@ public class AbsInteger {
 
     public AbsInteger(int num) {
         this.num=num;
-
     }
-
     public static void main(String[] args) {
-
         AbsInteger num3 = new ByteInteger((byte) 456);
         AbsInteger num4 = new ByteInteger((byte) 564);
-
         int b = num3.byteValue();
         int b1 = num4.byteValue();
         int sum1 = (b + b1);
-
         System.out.println(sum1);
-
     }
-
     public byte byteValue() {
         return (byte) num;
     }
-
-        static AbsInteger add(AbsInteger num1, AbsInteger num2) {
+    public int intValue(){
+        return  num;
+    }
+    public short shortValue(){
+        return (short) num;
+    }
+    static AbsInteger add(AbsInteger num1, AbsInteger num2) {
         num1 = num1;
         num2 = num2;
+        int result=0;
+        int a = num1.intValue();
+        int a1 = num2.intValue();
+        int sum = (a + a1);
 
-        int a = num1.byteValue();
-        int b = num2.byteValue();
-        int sum = (a + b);
+        int b = num1.byteValue();
+        int b1 = num2.byteValue();
+        int sum1 = (b + b1);
 
+        int c = num1.shortValue();
+        int c1 = num2.shortValue();
+        int sum2 = (c + c1);
+
+        if  (result == (sum | sum1 | sum2)) {
+        }
        return new ByteInteger((byte) sum);
 
-        }
     }
-
-    class ByteInteger extends AbsInteger {
+    }
+     class ByteInteger extends AbsInteger {
         byte num;
-
+        @Override
+        public int intValue(){
+            return  super.intValue();
+        }
         @Override
         public byte byteValue() {
             return super.byteValue();
         }
+        @Override
+        public short shortValue(){
+                return super.shortValue();
+            }
 
         public ByteInteger(byte num) {
             super(num);
-
             this.num = num;
         }
-
         @Override
         public String toString() {
             return "" + num;
         }
     }
-
     class ShortInteger extends AbsInteger {
-            byte num;
-
+            short num;
+        @Override
+        public int intValue(){
+            return  super.intValue();
+        }
         @Override
         public byte byteValue() {
             return super.byteValue();
         }
-
-        public ShortInteger(byte num) {
-            super((byte) num);
-
-            this.num = (byte) num;
+        @Override
+        public short shortValue(){
+            return super.shortValue();
         }
-
+        public ShortInteger(short num) {
+            super( num);
+            this.num =  num;
+        }
         @Override
         public String toString() {
             return "" + num;
         }
     }
-
     class IntInteger extends AbsInteger {
-        byte num;
-
+        int num;
+        @Override
+        public int intValue(){
+            return  super.intValue();
+        }
         @Override
         public byte byteValue() {
             return super.byteValue();
         }
-
-        public IntInteger(byte num) {
-            super((byte) num);
-
+        @Override
+        public short shortValue(){
+            return super.shortValue();
+        }
+        public IntInteger(int num) {
+            super(num);
             this.num = num;
         }
-
         @Override
         public String toString() {
             return "" + num;
