74374af749a7f145bef71bbbb66856fb6d3b39ea
Twist328 <evgen-shevel@yandex.ru>
2020-10-11 09:52:49 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/IntInteger.java
+++ src/ru/progwards/java1/lessons/bigints/IntInteger.java
@@ -17,13 +17,15 @@ class IntInteger extends AbsInteger {
         return "" + num;
     }
 
-    static AbsInteger add(AbsInteger num1, AbsInteger num2) {
-        num1 = num1;
-        num2 = num2;
-        int a = num1.intValue();
-        int b = num2.intValue();
-
-        return new IntInteger(a + b);
+    static AbsInteger add(AbsInteger num5, AbsInteger num6) {
+        num5 = num5;
+        num6 = num6;
+        int a = num5.intValue();
+        int b = num6.intValue();
+        int result = (a+b);
+        if (result <= -32768 & result <= 32767) {
+        }
+        return new IntInteger( result);
 
     }
 }
--- src/ru/progwards/java1/lessons/bigints/ShortInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ShortInteger.java
@@ -28,11 +28,11 @@ class ShortInteger extends AbsInteger {
         return "" + num;
     }
 
-    static AbsInteger add(AbsInteger num1, AbsInteger num2) {
-        num1 = num1;
-        num2 = num2;
-        int a = num1.shortValue();
-        int b = num2.shortValue();
+    static AbsInteger add(AbsInteger num3, AbsInteger num4) {
+        num3 = num3;
+        num4 = num4;
+        int a = num3.shortValue();
+        int b = num4.shortValue();
         int result = (a + b);
 
         if (result <= -128 & result >= -32768 & result > 127 & result <= 32767) {
