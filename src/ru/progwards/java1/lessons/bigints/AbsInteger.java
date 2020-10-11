b7bd50bc9179f7c0c758bfadf3a178ebe1301b1a
Twist328 <evgen-shevel@yandex.ru>
2020-10-11 19:58:17 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -20,18 +20,20 @@ public abstract class AbsInteger {
     }
 
     static AbsInteger add(AbsInteger num1, AbsInteger num2) {
+        num1=num1;
+        num2=num2;
         int a = num1.intValue();
         int b = num2.intValue();
         int result = (a + b);
 
-        if (result > (-128) | result <= 127) {
+        if (result > (-128) & result <= 127) {
             return new ByteInteger((byte) result);
-        } else if (result <= (-128) | result <= (-32768) | result > 127 | result <= 32767) {
+        } else if (result < (-128) & result <= (-32768)&result > 127 & result <= 32767){
             return new ShortInteger((short) result);
-        } else {
-
+        //} else if( result > 127 | result <= 32767) {
+           // return new ShortInteger((short) result);
+        }else {
             return new IntInteger(result);
-
         }
     }
 }
--- src/ru/progwards/java1/lessons/bigints/TestInteger.java
+++ src/ru/progwards/java1/lessons/bigints/TestInteger.java
@@ -6,7 +6,7 @@ public class TestInteger {
     public static void main(String[] args) {
 
         AbsInteger num1 = new IntInteger( 33000);
-        AbsInteger num2 = new ShortInteger((short) 32000);
+        AbsInteger num2 = new ShortInteger((short) 32700);
         AbsInteger num3 = new ShortInteger((short) 20588);
         AbsInteger num4 = new ByteInteger((byte) -126);
         AbsInteger num5 = new ByteInteger((byte) -126);
@@ -19,12 +19,15 @@ public class TestInteger {
         int b1 = num4.intValue();
         int a2 = num5.intValue();
         int b2 = num6.intValue();
+
         AbsInteger result  =  add(num1,num2);
-        System.out.println(result);
+
+
         int sum = (a + b);
         int sum1 = (a1 + b1);
         int sum2 = (a2 + b2);
 
+        System.out.println(result);
         System.out.println(sum);
         System.out.println(sum1);
         System.out.println(sum2);
