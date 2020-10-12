12c3bb58f35c3a4fde65d9b28b697b6ffed879e1
Twist <evgen-shevel@yandex.ru>
2020-10-12 17:16:33 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/TestInteger.java
+++ src/ru/progwards/java1/lessons/bigints/TestInteger.java
@@ -4,33 +4,28 @@ import static ru.progwards.java1.lessons.bigints.AbsInteger.add;
 
 public class TestInteger {
     public static void main(String[] args) {
-
-        AbsInteger num1 = new IntInteger( 33000);
-        AbsInteger num2 = new ShortInteger((short) -32758);
-        AbsInteger num3 = new ShortInteger((short) 20588);
-        AbsInteger num4 = new ByteInteger((byte) -126);
-        AbsInteger num5 = new ByteInteger((byte) -126);
-        AbsInteger num6 = new IntInteger( 100000);
+        AbsInteger num1 = new IntInteger( 111157);
+        AbsInteger num2 = new ShortInteger((short)-8000);
 
 
         int a = num1.intValue();
         int b = num2.intValue();
-        int a1 = num3.intValue();
-        int b1 = num4.intValue();
-        int a2 = num5.intValue();
-        int b2 = num6.intValue();
 
         AbsInteger result  =  add(num1,num2);
-
-
-        int sum = (a + b);
-        int sum1 = (a1 + b1);
-        int sum2 = (a2 + b2);
-
         System.out.println(result);
-        System.out.println(sum);
-        System.out.println(sum1);
-        System.out.println(sum2);
+
+        int res = (a + b);
+
+
+        if (res>-128&res<127){
+            System.out.println(res+"    ByteInteger");
+        } else if (res < -128 | res <= -32768) {
+            System.out.println(res+"    ShortInteger");
+        } else if( res > 127 & res <= 32767) {
+            System.out.println(res+"    ShortInteger");
+        }else {
+            System.out.println(res+"    IntInteger");
+        }
     }
 }
 
