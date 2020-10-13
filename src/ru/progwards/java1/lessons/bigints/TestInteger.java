49dcf63ca9f7b953a43abefc4df81b06d7286c92
Twist <evgen-shevel@yandex.ru>
2020-10-13 09:55:45 +0300

    AVK

--- src/ru/progwards/java1/lessons/bigints/TestInteger.java
+++ src/ru/progwards/java1/lessons/bigints/TestInteger.java
@@ -4,14 +4,13 @@ import static ru.progwards.java1.lessons.bigints.AbsInteger.add;
 
 public class TestInteger {
     public static void main(String[] args) {
-        AbsInteger num1 = new ByteInteger((byte) 69);
+        AbsInteger num1 = new ShortInteger((short) 16669);
         AbsInteger num2 = new IntInteger( -72678);
-
 
         int a = num1.intValue();
         int b = num2.intValue();
 
-        AbsInteger result = add(num1, num2);
+        AbsInteger result = add(num1,num2);
         System.out.println(result);
 
         int res = (a + b);
