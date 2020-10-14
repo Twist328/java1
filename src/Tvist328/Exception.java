19fd401fee896766b8231dc485b914c6b0c02c12
Twist328 <evgen-shevel@yandex.ru>
2020-10-14 22:45:25 +0300

    AVK

---
+++ src/Tvist328/Exception.java
@@ -0,0 +1,19 @@
+package Tvist328;
+
+class Exception1 {
+   public  Integer sqr(Integer n) {// ОБРАБОТКА ИСКЛЮЧЕНИЯ ПРИ МЕТОДЕ вычисляет квадрат переданного числа
+        try {
+            int x = n;
+            int res = 0;
+            res = x * x;
+            return res;
+        } catch (NullPointerException e) {
+
+        } finally {
+            if (n == null)
+                return -1; //ЗДЕСЬ МОЖЕТ БЫТЬ ЛЮБОЕ ЗАДАННОЕ ЗНАЧЕНИЕ КОТОРОЕ НУЖНО ВЕРНУТЬ ПРИ В ИСКЛЮЧЕНИЯ NullPointerException
+        }
+        return n;
+    }
+}
+
