7fae04a8a6aae94be93d7a02b9df8812d696a690
Twist328 <evgen-shevel@yandex.ru>
2020-10-15 07:59:56 +0300

    AVK

--- src/Tvist328/Exception.java
+++ src/Tvist328/Exception.java
@@ -1,7 +1,10 @@
 package Tvist328;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 class Exception1 {
-   public  Integer sqr(Integer n) {// ОБРАБОТКА ИСКЛЮЧЕНИЯ ПРИ МЕТОДЕ вычисления квадрата переданного числа
+    public Integer sqr(Integer n) {// ОБРАБОТКА ИСКЛЮЧЕНИЯ ПРИ МЕТОДЕ вычисления квадрата переданного числа
         try {
             int x = n;
             int res = 0;
@@ -12,9 +15,29 @@ class Exception1 {
         } finally {
             if (n == null)
                 return -1; //ЗДЕСЬ МОЖЕТ БЫТЬ ЛЮБОЕ ЗАДАННОЕ ЗНАЧЕНИЕ КОТОРОЕ НУЖНО ВЕРНУТЬ
-                           // ПРИ возникновении ИСКЛЮЧЕНИЯ NullPointerException
+            // ПРИ возникновении ИСКЛЮЧЕНИЯ NullPointerException
         }
         return n;
     }
-}
+
+    //public static void main(String[] args) {
+    public String test(String filename) throws IOException {
+        try {
+
+        } catch (NullPointerException e) {
+
+        } finally {
+
+            if (filename == null) {
+                return "File not found";
+
+            } else {
+
+                    return "File processing";
+                }
+            }
+        }
+    }
+
+
 
