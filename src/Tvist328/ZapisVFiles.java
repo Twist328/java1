613721c52fe7dae4097b60130d6059f6fc9c72f1
Twist328 <evgen-shevel@yandex.ru>
2020-10-21 22:12:53 +0300

    AVK

--- src/Tvist328/ZapisVFiles.java
+++ src/Tvist328/ZapisVFiles.java
@@ -7,29 +7,28 @@ import java.util.stream.Stream;
 
 public class ZapisVFiles {
     private static int lineCount(String filename) throws NumberFormatException, IOException {
+        int lineCount = 0;
         try {
-            int lineCount = 0, commentsCount = 0;
+
             Scanner input = new Scanner(new File(filename));
             while (input.hasNextLine()) {
                 String data = input.nextLine();
 
-                if (data.startsWith("//")) commentsCount++;
                 lineCount++;
             }
             return lineCount;
         } catch (FileNotFoundException e) {
             System.out.println("файл не найден");
         }
-        return 0;
+        return Integer.parseInt(filename);
     }
 
-
-    public static void main (String[]args) throws  IOException {
+    public static void main(String[] args) throws IOException {
         ZapisVFiles filename = new ZapisVFiles();
-            if (filename ==null)
+        //if (filename == null)
             System.out.println("файл не найден");
             System.out.println(lineCount(String.valueOf((1))));
-            }
+        }
     }
 
 
