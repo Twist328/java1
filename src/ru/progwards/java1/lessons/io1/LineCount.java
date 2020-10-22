2f05e46669d04b642c894c0c6d23bee8cadacbd1
Twist328 <evgen-shevel@yandex.ru>
2020-10-22 17:34:04 +0300

    AVK

--- src/ru/progwards/java1/lessons/io1/LineCount.java
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -1,8 +1,30 @@
 package ru.progwards.java1.lessons.io1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
 public class LineCount {
-    public static int calcEmpty(String fileName) {
-        return 0;
+    public static int calcEmpty(String fileName) throws FileNotFoundException {
+        int calcEmpty = 0;
+        try {
+            Scanner input = new Scanner(new File(fileName));
+            while (input.hasNextLine()) {
+                String data = input.nextLine();
+                calcEmpty++;
+            }
+            return calcEmpty;
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException(String.valueOf(-1));
+        }
     }
 
+    public static void main(String[] args) throws IOException {
+        LineCount fileName = new LineCount();
+        throw new NullPointerException(String.valueOf(-1));
+
+    }
 }
