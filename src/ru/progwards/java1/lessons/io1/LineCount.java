f69cd4ad4587788a6a00677dd3dc8ba5b29bbd54
Twist328 <evgen-shevel@yandex.ru>
2020-10-22 21:38:24 +0300

    ADK

--- src/ru/progwards/java1/lessons/io1/LineCount.java
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -4,29 +4,30 @@ import java.io.*;
 import java.util.Scanner;
 
 public class LineCount {
-    public static int calcEmpty(String fileName) throws NumberFormatException,IOException {
-        int empty = 0;
+    public static int calcEmpty(String fileName) throws NumberFormatException, IOException {
         try {
+            int empty = 0;
+
             final BufferedReader br = new BufferedReader(new StringReader("hello\n\nworld\n\ni\n\nam\n\npiter\n\n!"));
+
             String line;
 
             while ((line = br.readLine()) != null) {
                 if (line.trim().isEmpty()) {
                     empty++;
                 }
-                System.out.print(line);
-                }
-
-            } catch(NumberFormatException e){
-                throw new NumberFormatException(String.valueOf(-1));
             }
+            System.out.println(empty);
+            throw new NumberFormatException(String.valueOf("-1"));
+        } finally {
 
-        return empty;
+        }
     }
-        public static void main(String[] args) throws IOException,NumberFormatException {
+
+    public static void main(String[] args) throws IOException, NumberFormatException {
         LineCount calcEmpty = new LineCount();
-           // calcEmpty(String.valueOf(8));
+        // calcEmpty(String.valueOf(8));
         //throw new NumberFormatException(String.valueOf(-1));
-        System.out.println(calcEmpty(String.valueOf(100)));
+        System.out.println(calcEmpty(String.valueOf(1)));
     }
 }
