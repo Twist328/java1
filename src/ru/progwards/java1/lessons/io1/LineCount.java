31bf61e7191e6ddbcd6df1a73256a274c6aa82e8
Twist328 <evgen-shevel@yandex.ru>
2020-10-22 22:05:39 +0300

    ADK

--- src/ru/progwards/java1/lessons/io1/LineCount.java
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -5,24 +5,23 @@ import java.util.Scanner;
 
 public class LineCount {
     public static int calcEmpty(String fileName) throws NumberFormatException, IOException {
+        int empty = 0;String line;
+
         try {
-            int empty = 0;
-
             final BufferedReader br = new BufferedReader(new StringReader("hello\n\nworld\n\ni\n\nam\n\npiter\n\n!"));
-
-            String line;
-
             while ((line = br.readLine()) != null) {
                 if (line.trim().isEmpty()) {
                     empty++;
                 }
             }
+             }catch (Exception e){
             System.out.println(empty);
             throw new NumberFormatException(String.valueOf("-1"));
         } finally {
             if (fileName == String.valueOf(0))
                 return -1;
         }
+        return empty;
     }
 
     public static void main(String[] args) throws IOException, NumberFormatException {
