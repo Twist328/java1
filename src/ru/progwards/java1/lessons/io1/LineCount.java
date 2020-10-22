85b1a34dec31e4875601ca3cffdf9324d7b0e7f2
Twist328 <evgen-shevel@yandex.ru>
2020-10-22 18:46:05 +0300

    ADK

--- src/ru/progwards/java1/lessons/io1/LineCount.java
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -1,15 +1,23 @@
 package ru.progwards.java1.lessons.io1;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 import static java.lang.System.*;
 
 public class LineCount {
     public static int calcEmpty(String fileName) throws IOException {
-        int calcEmpty = 0;
+        final BufferedReader br = new BufferedReader(new StringReader("hello\n\nworld\n"));
+        String line;
+        int empty = 0;
+        while ((line = br.readLine()) != null) {
+            if (line.trim().isEmpty()) {
+                empty++;
+            }
+        }
+        System.out.println(empty);
+
+       /*/ int calcEmpty = 0;
         try {
             Scanner input = new Scanner(new File(fileName));
             while (input.hasNextLine()) {
@@ -18,10 +26,10 @@ public class LineCount {
             }
             return calcEmpty;
         } catch (IOException e) {
-            throw new IOException(String.valueOf(-1));
-        }
-    }
+            throw new IOException(String.valueOf(-1));/*/
 
+        return empty;
+    }
     public static void main(String[] args) throws IOException {
         LineCount fileName = new LineCount();
         throw new IOException(String.valueOf(-1));
