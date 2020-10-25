c36df175aeb761bf49b40b3e997cee2fcd5f4bc7
Twist328 <evgen-shevel@yandex.ru>
2020-10-25 18:08:37 +0300

    ADK

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -1,20 +1,20 @@
 package ru.progwards.java1.lessons.bigints;
 
-        import java.lang.reflect.Array;
-        import java.math.BigInteger;
-        import java.util.ArrayList;
-        import java.util.Arrays;
-        import java.util.Collections;
-        import java.util.List;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-        import static java.util.Arrays.*;
+import static java.util.Arrays.*;
 
 public class ArrayInteger {
 
     private final static BigInteger BI_TEN = BigInteger.valueOf(10);
     private Byte[] digits;
 
-    public ArrayInteger( int n) {
+    public ArrayInteger(int n) {
         digits = new Byte[n];
     }
 
@@ -28,61 +28,188 @@ public class ArrayInteger {
         digits = list.toArray(new Byte[list.size()]);
     }
 
-    public BigInteger toInt() {
-        BigInteger result = BigInteger.ZERO;
-        for (Byte b : digits) {
-            result = result.multiply(BI_TEN);
-            result = result.add(BigInteger.valueOf(b));
+    BigInteger toInt() {
+        BigInteger bigInteger = new BigInteger("0");
+        if (digits.length == 0) {
+            return bigInteger;
+        }
+        if (digits.length == 1) {
+            return new BigInteger(String.valueOf(digits[0]));
+        }
+        for (int i = 0; i < digits.length; i++) {
+            bigInteger = bigInteger.add(new BigInteger(String.valueOf(new BigInteger(String.valueOf(digits[i])).
+                    multiply(BigInteger.TEN.pow(i)))));
+        }
+        return bigInteger;
+    }
+
+   public boolean add(ArrayInteger num) {
+        boolean result = false;
+        if (digits.length < num.digits.length) {
+            for (int i = 0; i < digits.length; i++) {
+                digits[i] = 0;
+            }
+            return false;
+        }
+        if (digits.length == num.digits.length) {
+            for (int i = 0; i < digits.length; i++) {
+                byte b = (byte) (digits[i] + num.digits[i]);
+                if (b >= 10 & i == digits.length - 1) {
+                    for (int j = 0; j < digits.length; j++) {
+                        digits[j] = 0;
+                    }
+                    return false;
+                }
+                if (b >= 10) {
+                    digits[i] = (byte) (b % 10);
+                    digits[i + 1] = (byte) (digits[i + 1] + (b / 10));
+                } else {
+                    digits[i] = b;
+                }
+            }
+            result = true;
+        }
+        if (digits.length > num.digits.length) {
+            for (int i = 0; i < num.digits.length; i++) {
+                byte b = (byte) (digits[i] + num.digits[i]);
+                if (b >= 10 & digits[num.digits.length] == 9 & digits.length == num.digits.length + 1) {
+                    for (int j = 0; j < digits.length; j++) {
+                        digits[j] = 0;
+                    }
+                    return false;
+                }
+                if (b >= 10) {
+                    digits[i] = (byte) (b % 10);
+                    digits[i + 1] = (byte) (digits[i + 1] + (b / 10));
+                } else {
+                    digits[i] = b;
+                }
+            }
+            for (int i = 0; i < digits.length; i++) {
+                if (digits[i] > 9 & i == digits.length - 1) {
+                    for (int j = 0; j < digits.length; j++) {
+                        digits[i] = 0;
+                    }
+                } else if (digits[i] > 9) {
+                    digits[i + 1] = (byte) (digits[i + 1] + digits[i] / 10);
+                    digits[i] = (byte) (digits[i] % 10);
+                }
+            }
+            result = true;
+        }
+        return result;
+    }
+}
+    /*/public static void main(String[] args) {
+        ArrayInteger arrayInteger = new ArrayInteger(9);
+       System.out.println(Arrays.toString(arrayInteger.digits));
+        arrayInteger.fromInt(new BigInteger("89289799"));
+        System.out.println(Arrays.toString(arrayInteger.digits));
+        System.out.println(arrayInteger.toInt());
+        ArrayInteger arrayInteger1 = new ArrayInteger(5);
+        arrayInteger1.fromInt(new BigInteger("805097"));
+        System.out.println(arrayInteger.add(arrayInteger1));
+        System.out.println(Arrays.toString(arrayInteger.digits));
+    }
+}*/
+/*/public class ArrayInteger {
+    byte[] digits;
+
+    ArrayInteger(int n) {
+        digits = new byte[n];
+    }
+
+    void fromInt(BigInteger value) {
+        if (value.compareTo(BigInteger.TEN) == -1) {
+            digits[0] = value.byteValue();
+        }
+        for (int i = 0; value.compareTo(new BigInteger("0")) != 0; i++) {
+            digits[i] = value.mod(BigInteger.TEN).byteValue();
+            value = value.divide(BigInteger.TEN);
+        }
+    }
+
+    BigInteger toInt() {
+        BigInteger bigInteger = new BigInteger("0");
+        if (digits.length == 0) {
+            return bigInteger;
+        }
+        if (digits.length == 1) {
+            return new BigInteger(String.valueOf(digits[0]));
+        }
+        for (int i = 0; i < digits.length; i++) {
+            bigInteger = bigInteger.add(new BigInteger(String.valueOf(new BigInteger(String.valueOf(digits[i])).
+                    multiply(BigInteger.TEN.pow(i)))));
+        }
+        return bigInteger;
+    }
+
+    boolean add(ArrayInteger num) {
+        boolean result = false;
+        if (digits.length < num.digits.length) {
+            for (int i = 0; i < digits.length; i++) {
+                digits[i] = 0;
+            }
+            return false;
+        }
+        if (digits.length == num.digits.length) {
+            for (int i = 0; i < digits.length; i++) {
+                byte number = (byte) (digits[i] + num.digits[i]);
+                if (number >= 10 & i == digits.length - 1) {
+                    for (int j = 0; j < digits.length; j++) {
+                        digits[j] = 0;
+                    }
+                    return false;
+                }
+                if (number >= 10) {
+                    digits[i] = (byte) (number % 10);
+                    digits[i + 1] = (byte) (digits[i + 1] + (number / 10));
+                } else {
+                    digits[i] = number;
+                }
+            }
+            result = true;
+        }
+        if (digits.length > num.digits.length) {
+            for (int i = 0; i < num.digits.length; i++) {
+                byte number = (byte) (digits[i] + num.digits[i]);
+                if (number >= 10 & digits[num.digits.length] == 9 & digits.length == num.digits.length + 1) {
+                    for (int j = 0; j < digits.length; j++) {
+                        digits[j] = 0;
+                    }
+                    return false;
+                }
+                if (number >= 10) {
+                    digits[i] = (byte) (number % 10);
+                    digits[i + 1] = (byte) (digits[i + 1] + (number / 10));
+                } else {
+                    digits[i] = number;
+                }
+            }
+            for (int i = 0; i < digits.length; i++) {
+                if (digits[i] > 9 & i == digits.length - 1) {
+                    for (int j = 0; j < digits.length; j++) {
+                        digits[i] = 0;
+                    }
+                } else if (digits[i] > 9) {
+                    digits[i + 1] = (byte) (digits[i + 1] + digits[i] / 10);
+                    digits[i] = (byte) (digits[i] % 10);
+                }
+            }
+            result = true;
         }
         return result;
     }
 
-    public boolean add(ArrayInteger num) {
-        int ten = 0;
-        final List<Byte> list = new ArrayList<>();
-        for (int i = Math.max(num.digits.length, this.digits.length) - 1; i >= 0; i--) {
-            int result = ((num.digits.length <= i) ? 0 : num.digits[i]) +
-                    ((this.digits.length <= i) ? 0 : this.digits[i]) + ten;
-            ten = result > 9 ? 1 : 0;
-            list.add((byte) (result % 10));
-        }
-        Collections.reverse(list);
-        digits = list.toArray(new Byte[list.size()]);
-        return true;
+    public static void main(String[] args) {
+        ArrayInteger arrayInteger = new ArrayInteger(8);
+//        System.out.println(Arrays.toString(arrayInteger.digits));
+        arrayInteger.fromInt(new BigInteger("15980465"));
+        System.out.println(Arrays.toString(arrayInteger.digits));
+        System.out.println(arrayInteger.toInt());
+        ArrayInteger arrayInteger1 = new ArrayInteger(5);
+        arrayInteger1.fromInt(new BigInteger("69268"));
+        System.out.println(arrayInteger.add(arrayInteger1));
+        System.out.println(Arrays.toString(arrayInteger.digits));
     }
-
-    @Override
-    public String toString() {
-        return digits == null ? null : Arrays.toString(digits);
-    }
-
-}
-/*/import java.math.BigInteger;
-
-public class ArrayInteger {
-    byte[] digits;
-    int putVal;
-    int getVal;
-
-    ArrayInteger(int n){
-        digits = new byte[n];
-        putVal = 0;
-        getVal = 0;
-    }
-
-    void fromInt(BigInteger value){
-        digits[putVal++] = value.byteValueExact();
-    }
-
-    BigInteger toInt(){
-        byte[] rev = new byte[digits.length + 1];
-        if (getVal == putVal){
-            return BigInteger.ZERO;
-        }
-        return new BigInteger(rev);
-    }
-
-    boolean add(ArrayInteger num){
-        return true;
-    }
-/*/ //}
+}*/
