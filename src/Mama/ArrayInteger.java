2b20cfe5f783d658764ce5a8fe14633ba6390132
Twist <evgen-shevel@yandex.ru>
2020-10-30 14:24:46 +0300

    ASK

--- src/Mama/ArrayInteger.java
+++ src/Mama/ArrayInteger.java
@@ -1,11 +1,12 @@
 package Mama;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+
 
 public class ArrayInteger {
     private static BigInteger BI_TEN = BigInteger.valueOf(10);
     private Byte[] digits;
-
     public ArrayInteger(int n) {
         digits = new Byte[n];
     }
@@ -24,14 +25,12 @@ public class ArrayInteger {
         BigInteger bigInteger = new BigInteger("0");
         if (digits.length == 0) {
             return bigInteger;
-
         }
         if (digits.length == 1) {
             return new BigInteger(String.valueOf(digits[0]));
         }
         for (int i = 0; i < digits.length; i++) {
             bigInteger = bigInteger.add(new BigInteger(String.valueOf(new BigInteger(String.valueOf(digits[i])).multiply(BigInteger.TEN.pow(i)))));
-
         }
         return bigInteger;
     }
@@ -41,7 +40,6 @@ public class ArrayInteger {
         if (digits.length < num.digits.length) {
             for (int i = 0; i < digits.length; i++) {
                 digits[i] = 0;
-
             }
             return false;
         }
@@ -52,10 +50,64 @@ public class ArrayInteger {
                     for (int j = 0; j < digits.length; j++) {
                         digits[j] = 0;
                     }
-                     return false;
+                    return false;
+                }
+
+                if (b >= 10) {
+                    digits[i] = (byte) (b % 10);
+                    digits[i + 1] = (byte) (digits[i + 1] + (b / 10));
+                } else {
+                    digits[i] = b;
+                }
+                return true;
+            }
+            if (digits.length > num.digits.length) {
+                for (int i = 0; i < num.digits.length; i++) {
+                    byte b = (byte) (digits[i] + num.digits[i]);
+                    if (b >= 10 & digits[num.digits.length] == 9 & digits.length == num.digits.length + 1) {
+                        for (int j = 0; j < digits.length; j++) {
+                            digits[j] = 0;
+                        }
+                        return false;
+                    }
+                    if (b >= 10) {
+                        digits[i] = (byte) (b % 10);
+                        digits[i + 1] = (byte) (digits[i + 1] + (b / 10));
+                    } else {
+                        digits[i] = b;
                     }
                 }
+                for (int i = 0; i < digits.length; i++) {
+                    if (digits[i] > 9 & i == digits.length - 1) {
+                        for (int j = 0; j < digits.length; j++) {
+                            digits[i] = 0;
+                        }
+
+                    } else if (digits[i] > 9) {
+                        digits[i + 1] = (byte) (digits[i + 1] + digits[i] / 10);
+                        digits[i] = (byte) (digits[i] % 10);
+                    }
+
+                    result = true;
                 }
+                return result;
+            }
+        }
+        return result;
+    }
+            public static void main (String[]args){
+                ArrayInteger arrayInteger = new ArrayInteger(9);
+                arrayInteger.fromInt(new BigInteger("89289799"));
+                ArrayInteger arrayInteger1 = new ArrayInteger(5);
+                //arrayInteger1.fromInt(new BigInteger("805097"));
+                //System.out.println(arrayInteger.toInt());
+                //System.out.println(arrayInteger.add(arrayInteger1));
+                System.out.println(Arrays.toString(arrayInteger.digits));
+
+            }
+        }
+
+
 
 
 
