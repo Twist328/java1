56361e54e42e3c98f6a44e16e8c954bbc396a9a8
Twist328 <evgen-shevel@yandex.ru>
2020-11-01 20:42:05 +0300

    Sc

--- src/Mama/Fileblabla.java
+++ src/Mama/Fileblabla.java
@@ -1,42 +1,43 @@
 package Mama;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 class Scannerblabla {
-    public void scanLines() {
+    public static void scanLines() {
         String str = "Привет!";
         String str1 = "как дела?";
         String str2 = "/stop";
-        String str3 = "Как погода?";
-        String str4 = "Мой друг, Привет тебе, привет!";
-        String str5 = "Как настроение?";
-        String str6 = "У тебя сейчас как дела??";
+        // String str2 = "Как погода?\nМой друг, Привет тебе, привет!\nКак настроение?\nУ тебя сейчас как дела?";
+        String str3 = "Здравствуйте!";
+        String str4 = "Как погода?";
+        String str5 = "Хорошо";
+
         try (Scanner scanner = new Scanner(System.in)) {
-            if (str2 == scanner.nextLine()) {
+
+            str = scanner.nextLine();
+            System.out.println(str3);
+            str1 = scanner.nextLine();
+            System.out.println(str5);
+            str4=scanner.nextLine();
+            System.out.println(str4);
+            while (str2 == "/stop") {
                 scanner.close();
-                System.out.println("/stop");
-                str = scanner.nextLine();
-                System.out.println("Здравствуйте!");
-                str1 = scanner.nextLine();
-                System.out.println("Хорошо");
-                try {
-                    if (str3 == scanner.nextLine()) {
-                        System.out.println("Как погода?");
-                        System.out.println("Здравствуйте!");
-                        System.out.println("Как настроение?");
-                        System.out.println("Хорошо");
-                        while (str2 == scanner.nextLine()) {
-                            scanner.close();
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
             }
+                System.out.println(str4);
+                str1 = scanner.nextLine();
+                scanner.close();
+            }
+
         }
+
+    public static void main(String[] args) {
+        Scannerblabla scannerblabla=new Scannerblabla();
+        scanLines();
+        System.out.println(scannerblabla);
     }
 }
-
 
 // boolean contains(String str){
 
