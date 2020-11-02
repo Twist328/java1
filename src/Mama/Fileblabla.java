10b90463e19335f9c8fd0b7530356a0282fd7b87
Twist <evgen-shevel@yandex.ru>
2020-11-02 17:08:58 +0300

    ASK

--- src/Mama/Fileblabla.java
+++ src/Mama/Fileblabla.java
@@ -6,49 +6,59 @@ import java.util.Scanner;
 
 class Scannerblabla {
     public static void scanLines() {
-        String str = "Привет!";
+        String str = "Привет!\nкак дела?";
         String str1 = "как дела?";
         String str2 = "/stop";
         String str6 = "Как погода?\nМой друг, Привет тебе, привет!\nКак настроение?\nУ тебя сейчас как дела?";
-        String str3 = "Здравствуйте!";
+        String str3 = "Здравствуйте!\nХорошо";
         String str4 = "Как погода?";
         String str5 = "Хорошо";
         String str7 = "Как погода?\nЗдравствуйте!\nКак настроение?\nХорошо";
 
-
         try (Scanner sc = new Scanner(System.in)) {
-            while (true) {
-                str = sc.nextLine();
-                System.out.println(str3);
-                str1 = sc.nextLine();
-                System.out.println(str5);
-                if (str2 == String.valueOf(false)) ;
-                sc.close();
-                break;
-            }
 
+                while (sc.hasNextLine()) {
+                    str = sc.nextLine();
+                    System.out.println(str3);
+                    str2 = sc.nextLine();
+                    sc.close();
 
                     try (Scanner scan = new Scanner(System.in)) {
-                        str6 = scan.nextLine();
-                        System.out.println(str7);
-                        str2 = scan.nextLine();
-                        System.out.println();
-                        scan.close();
+                        while (true) {
+                            while (scan.hasNext()) {
+                                str6 = scan.nextLine();
+                                System.out.println(str7);
+                                str2 = scan.nextLine();
+                                scan.close();
+                            }
+                   /*/ str = sc.nextLine();
+                    System.out.println(str3);
+                    str1 = sc.nextLine();
+                    System.out.println(str5);
 
+                    while (true) {
 
-
-                    }
-                }
-            }
-
-
-
-                            public static void main (String[]args){
-                            Scannerblabla scannerblabla = new Scannerblabla();
-                            scanLines();
-                            System.out.println(scannerblabla);
+                        try (Scanner scan = new Scanner(System.in)) {
+                            while (scan.hasNext()) {
+                                str6 = scan.nextLine();
+                                System.out.println(str7);
+                                str2 = Boolean.parseBoolean(scan.nextLine());
+                                System.out.println();*/
                         }
                     }
+                }
+
+            }
+        }
+
+
+
+    public static void main(String[] args) {
+        Scannerblabla scannerblabla = new Scannerblabla();
+        scanLines();
+        System.out.println(scannerblabla);
+    }
+}
 
 // boolean contains(String str){
 
