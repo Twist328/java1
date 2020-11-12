d47ae37f3ec627dd21ca6ca242408e492bf2bf10
Twist328 <evgen-shevel@yandex.ru>
2020-11-12 22:02:40 +0300

    Tr

--- src/ru/progwards/java1/lessons/io2/PhoneNumber.java
+++ src/ru/progwards/java1/lessons/io2/PhoneNumber.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class PhoneNumber {
     public static String format(String phone) throws IOException {
-        String numbers = phone.replaceAll("[^\\d]", "");
+        /*String numbers = phone.replaceAll("[^\\d]", "");
         int temp = numbers.length();
         if (temp > 11) {
             throw new RuntimeException("проверьте количество цифр в номере телефона");
@@ -13,8 +13,17 @@ public class PhoneNumber {
         }
         return "+" + (numbers.charAt(0) == '8' ? "7" : numbers.substring(0,1))
                 + "(" + numbers.substring(1, 4) + ")"
-                + numbers.substring(4, 7) + "-" + numbers.substring(7);//
+                + numbers.substring(4, 7) + "-" + numbers.substring(7);//*/
+        String numbers = phone.replaceAll("[^\\d]", "");
+        int temp = numbers.length();
+        if (temp > 11 || temp < 10) {
+            throw new RuntimeException("Нужно проверить количество цифр номера!");
+        }
+        return "+" + (numbers.charAt(0) == '8' ? "7" : numbers.substring(0, 1))
+                + "(" + numbers.substring(1, 4) + ")"
+                + numbers.substring(4, 7) + "-" + numbers.substring(7);
     }
+
 
     public static void main(String[] args) throws IOException {
         System.out.println(format("7(999)111-22-33"));
---
+++ src/ru/progwards/java1/lessons/io2/Translator.java
@@ -0,0 +1,58 @@
+package ru.progwards.java1.lessons.io2;
+
+import java.util.Arrays;
+
+public class Translator {
+    private String[] inLang;
+    private String[] outLang;
+
+    Translator(String[] inLang, String[] outLang) {
+        this.inLang = inLang;
+        this.outLang = outLang;
+    }
+
+    public String translate(String sentence) {
+        String[] arrSentence = sentence.split(" ");
+        String temp = "";
+        for (int i = 0; i < arrSentence.length; i++) {
+            String str = "";
+            for (char c :
+                    arrSentence[i].toCharArray()) {
+                if (Character.isAlphabetic(c)) {
+                    str += c;
+                }
+            }
+            str = str.toLowerCase();
+            if (str.equals("")) {
+                temp = temp + arrSentence[i] + " ";
+            } else {
+                for (int j = 0; j < inLang.length; j++) {
+                    if (str.equals(inLang[j])) {
+                        String next = "";
+                        if (arrSentence[i].substring(0, 1).equals(arrSentence[i].substring(0, 1).toUpperCase())) {
+                            next = outLang[j].substring(0, 1).toUpperCase() + outLang[j].substring(1);
+                        } else {
+                            next = outLang[j];
+                        }
+                        if (str.length() == arrSentence[i].length()) {
+                            temp = temp + next + " ";
+                        } else {
+                            int substr = arrSentence[i].length() - (arrSentence[i].length() - str.length());
+                            temp = temp + next + arrSentence[i].substring(substr) + " ";
+                        }
+                    }
+                }
+            }
+        }
+        temp = temp.trim();
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        String[] inLang = {"what", "fell", "is", "lost"};
+        String[] outLang = {"что", "упало", "то", "пропало"};
+        String sentence = "What fell - is lost!";
+        Translator translator = new Translator(inLang, outLang);
+        System.out.println(translator.translate(sentence));
+    }
+}
