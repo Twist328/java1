bc0c6d257573001009e5b79a8e35212f8d4e8587
Twist328 <evgen-shevel@yandex.ru>
2020-11-19 23:26:53 +0300

    Tr

--- src/Mama/TesttoCharArray.java
+++ src/Mama/TesttoCharArray.java
@@ -6,9 +6,18 @@ public class TesttoCharArray {
             String str = "МАМА МЫЛА РАМУ";
             char[] result = str.toCharArray();
 
+            String str1 = "254137";
+            char[] result1 = str1.toCharArray();
+
             System.out.println("Char array:");
+
             for (int i = 0; i < result.length; i++)
-                System.out.println("Element [" + i + "]: " + result[i]);     /* ТО ЕСТЬ МЕТОД str.toCharArray()
+                System.out.println("Element [" + i + "]: " + result[i]);
+
+            for (int i = 0; i < result1.length; i++)
+                System.out.println("Element [" + i + "]: " + result1[i]);
+
+            /* ТО ЕСТЬ МЕТОД str.toCharArray()
             ПРЕОБРАЗОВАЛ СТРОКУ "МАМА МЫЛА РАМУ" В МАССИВ ЧАРОВ : {'МАМА', 'МЫЛА','РАМУ'}*/
         }
     }
