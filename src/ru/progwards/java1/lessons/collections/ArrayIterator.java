ede863776ca36b33728ec45b0b7ec5295eafb9b8
Twist328 <evgen-shevel@yandex.ru>
2020-11-22 18:51:43 +0300

    Tr

--- src/ru/progwards/java1/lessons/collections/ArrayIterator.java
+++ src/ru/progwards/java1/lessons/collections/ArrayIterator.java
@@ -3,25 +3,28 @@ package ru.progwards.java1.lessons.collections;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class ArrayIterator<T> implements Iterator<T> {
 
-    private T[] array;
 
-    ArrayIterator(T[] array) {
-        this.array = array;
+    public class ArrayIterator<T> implements Iterator<T> {
+        private T array[];
+        private int pos = 0;
+
+        public ArrayIterator(T anArray[]) {
+            array = anArray;
+        }
+
+        public boolean hasNext() {
+            return pos < array.length;
+        }
+
+        public T next() throws NoSuchElementException {
+            if (hasNext())
+                return array[pos++];
+            else
+                throw new NoSuchElementException();
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
-
-    @Override
-    public boolean hasNext() {
-
-        return false;
-    }
-
-    @Override
-    public T next() {
-        if (next().equals(0))
-            throw new NoSuchElementException();
-
-        return (T) this;
-    }
-}
