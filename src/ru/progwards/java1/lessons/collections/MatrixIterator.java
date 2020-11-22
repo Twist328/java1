d20cdc7fc01275b1682611cb0e52894003f8d842
Twist328 <evgen-shevel@yandex.ru>
2020-11-22 18:38:21 +0300

    Tr

--- src/ru/progwards/java1/lessons/collections/MatrixIterator.java
+++ src/ru/progwards/java1/lessons/collections/MatrixIterator.java
@@ -1,51 +1,51 @@
 package ru.progwards.java1.lessons.collections;
 
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class MatrixIterator<T> implements Iterator<T> {
-    private int skolkoel;          //всего элементов в матрице
+    private int size;          //всего элементов в матрице
     private int position = 0;  //номер текущего элемента для "выдачи"
-    private int stroka = 0;       //строка текущего элемента
-    private int colonka = 0;       //столбец текущего элемента
+    private int row = 0;       //строка текущего элемента
+    private int col = 0;       //столбец текущего элемента
     private T[][] matrix;
 
     public MatrixIterator(T[][] matrix) {
         this.matrix = matrix;
-        this.skolkoel = countElements(matrix);
+        this.size = countElements(matrix);
     }
 
     private int countElements(T[][] matrix) {  //считаем количество элементов в матрице
         int count = 0;
-        for (T[] stroka : matrix) {
-            count += stroka.length;
+        for (T[] row : matrix) {
+            count += row.length;
         }
         return count;
     }
 
     @Override
     public boolean hasNext() {
-        return position < skolkoel;
+        return position < size;
     }
 
     @Override
     public T next() {
-        if (position >= skolkoel) { //если перебрали все элементы, то бросить исключение
+        if (position >= size) { //если перебрали все элементы, то бросить исключение
             throw new NoSuchElementException();
         }
-        T element = matrix[stroka][colonka];  //запоминаем текущий элемент
+        T element = matrix[row][col];  //запоминаем текущий элемент
         //переходим к следующему элементу
         position++;
-        colonka++;
-        while (stroka < matrix.length && colonka >= matrix[stroka].length) { //для того, чтоб пропустить возможные "пустые" строки
-            colonka = 0;
-            stroka++;
+        col++;
+        while (row < matrix.length && col >= matrix[row].length) { //для того, чтоб пропустить возможные "пустые" строки
+            col = 0;
+            row++;
         }
         return element;
     }
 
     public static void main(String[] args) {
+
         Integer[][] matrix = {{1, 2, 3}, {}, {4, 5}, {6}};
         Iterator<Integer> iterator = new MatrixIterator<>(matrix);
         while (iterator.hasNext()) {
@@ -53,4 +53,5 @@ public class MatrixIterator<T> implements Iterator<T> {
         }
     }
 }
+
 
