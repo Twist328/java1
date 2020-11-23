67f24115bb55dd6b54383529b7a9052da7db7f3c
Twist328 <evgen-shevel@yandex.ru>
2020-11-23 22:12:24 +0300

    Tr

--- src/FactorialClass.java
+++ src/FactorialClass.java
@@ -1,15 +1,15 @@
-public class Posit2 {
+public class FactorialClass {
     static long factorial(long n) {
 
         long result = 1l;
         for (long i = 1l; i <= n; i++) {
-            result = result * i;
+            result *= i;
         }
         return result;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(factorial(17));
+        System.out.println(factorial(20));
     }
 }
--- src/Less4.java
+++ src/Less4.java
@@ -5,7 +5,6 @@ import java.math.RoundingMode;
 public class Less4 {
     static int addAsStrings(int n1, int n2) {
         String str = String.valueOf(n1);
-
         String str1 = String.valueOf(n2);
 
         return Integer.parseInt(str+str1);
@@ -15,8 +14,19 @@ public class Less4 {
 
     public static void main(String[] args) {
         System.out.println(addAsStrings(1, 2));
+        System.out.println(addAsStrings(2, 1));
         MathContext mathContext = new MathContext(3);
         BigDecimal result = new BigDecimal("3.3333", mathContext);
+
+        String a = "Не всяко ";
+        String b = "в строку";
+        String c = "лыко ";
+        System.out.println(a + c + b);
+
+        float d= (float) 1000000.0;
+        float f = (float)d;
+        System.out.println(f);
+
 
 
         BigDecimal bigDec1 = BigDecimal.valueOf(10);
