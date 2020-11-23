11bc24e7d85dd1a80dc65935006afc3bac576ca8
Twist328 <evgen-shevel@yandex.ru>
2020-11-23 22:57:47 +0300

    Tr

--- src/FactorialClass.java
+++ src/FactorialClass.java
@@ -11,5 +11,21 @@ public class FactorialClass {
 
     public static void main(String[] args) {
         System.out.println(factorial(20));
+
+        System.out.println(Math.pow(20l, 3));
+        //for ( ; ; ) {//вечный цикл
+
+
+        if (1 > 2 && 5 >= 0)
+
+            System.out.println("Что-то из этого условия точно верно");
+
+        else
+
+            System.out.println("Быть может что-то в этом условии верно, но что-то точно нет");
+
+        int i = (int)765.567;//отсекает остаток
+        System.out.println(i);
     }
 }
+
