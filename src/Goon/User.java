ea8fa95690711fb38c9651a8df6736e01843be99
Twist328 <evgen-shevel@yandex.ru>
2020-11-28 20:42:13 +0300

    Tr

--- src/Goon/User.java
+++ src/Goon/User.java
@@ -1,6 +1,5 @@
 package Goon;
 
-
 import java.util.Comparator;
 import java.util.TreeSet;
 
@@ -10,6 +9,15 @@ class User {//Создайте метод с сигнатурой public TreeSet
 
 
     public Integer id;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
     public String name;
 
     User(Integer id, String name) {
@@ -17,14 +25,34 @@ class User {//Создайте метод с сигнатурой public TreeSet
         this.name = name;
     }
 
-    public TreeSet<User> createSet() {
-        TreeSet<User>treeSet = new TreeSet<>(new Comparator<User>() {
+    public static TreeSet<User> createSet() {
+        TreeSet<User>treeSet = new TreeSet<User>(new Comparator<User>() {
             @Override
             public int compare(User o1, User o2) {
                 return  Integer.compare(o2.id, o1.id);
             }
         });
         return treeSet;
+    }
+
+    public static void main(String[] args) {
+
+        TreeSet<User> backsort = createSet();
+        backsort.add(new User(1, "Вася"));
+        backsort.add(new User(5, "Петя"));
+        backsort.add(new User(2, "Коля"));
+        backsort.add(new User(4, "Дима"));
+
+        System.out.println(backsort);
+
+        TreeSet<User> backsort1 = createSet();
+        backsort1.add(new User(11, "Маша"));
+        backsort1.add(new User(15, "Саша"));
+        backsort1.add(new User(23, "Соня"));
+        backsort1.add(new User(44, "Даша"));
+
+        System.out.println(backsort1);
+
     }
 }
 
