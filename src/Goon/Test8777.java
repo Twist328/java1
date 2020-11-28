c2301050850cf797988e7f6409825ea2ad48c345
Twist328 <evgen-shevel@yandex.ru>
2020-11-28 19:09:44 +0300

    Tr

---
+++ src/Goon/Test8777.java
@@ -0,0 +1,37 @@
+package Goon;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class Test8777 {
+
+    public static void doTreeSet() {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+    }
+
+    public static void main(String[] args) {
+
+        Test8777 test8777=new Test8777();
+        TreeSet<Integer> treeSet = new TreeSet<>();
+
+        treeSet.add(9);
+        treeSet.add(1);
+        treeSet.add(5);
+        treeSet.add(2);
+        treeSet.add(4);
+        treeSet.add(8);
+
+        String s = "";
+        Iterator<Integer> iterator = treeSet.descendingIterator();
+
+        while (iterator.hasNext()) ;
+        s += iterator.next();
+
+        System.out.println(s);
+
+    }
+}
+
+
+
+
