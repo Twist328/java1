0ecc751232980f43ca468ad7c7a1a34c2ea3e1b6
Twist328 <evgen-shevel@yandex.ru>
2020-11-28 20:58:18 +0300

    Tr

--- src/ru/progwards/java1/lessons/sets/SetOperations.java
+++ src/ru/progwards/java1/lessons/sets/SetOperations.java
@@ -1,4 +1,10 @@
 package ru.progwards.java1.lessons.sets;
 
+import java.util.Set;
+
 public class SetOperations {
+    public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2){
+
+        return set1;
+    }
 }
