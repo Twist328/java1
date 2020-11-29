658828450458e9b6ed31b0a650634185a28646e6
Twist328 <evgen-shevel@yandex.ru>
2020-11-29 14:10:42 +0300

    Tr

--- src/ru/progwards/java1/lessons/sets/SetOperations.java
+++ src/ru/progwards/java1/lessons/sets/SetOperations.java
@@ -8,45 +8,45 @@ import java.util.Set;
 public class SetOperations {
     public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
 
-        HashSet<Integer> result = new HashSet(set1);
-        result.addAll(set2);
-        return result;
+        HashSet<Integer> res = new HashSet(set1);
+        res.addAll(set2);
+        return res;
     }
 //_____________________________________________________________//
 
     public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
 
         if (set1 == null || set2 == null) return new HashSet<Integer>();
-        HashSet<Integer> result = new HashSet<Integer>(set1);
-        result.retainAll(set2);
+        HashSet<Integer> res = new HashSet<Integer>(set1);
+        res.retainAll(set2);
 
-        return result;
+        return res;
     }
 
     //________________________________________________________________________//
     public static Set<Integer> difference(Set<Integer> set1, Set<Integer> set2) {
 
         if (set1 == null) return new HashSet<Integer>();
-        HashSet<Integer> result = new HashSet<Integer>(set1);
-        if (set2 == null) result.removeAll(set2);
-        return result;
+        HashSet<Integer> res = new HashSet<Integer>(set1);
+        if (set2 != null) res.removeAll(set2);
+        return res;
     }
 //_______________________________________________________________________________________________________//
     public static Set<Integer> symDifference(Set<Integer> set1, Set<Integer> set2) {
 
         Set<Integer> res1 = set1 != null ? new HashSet<Integer>(set1) : new HashSet<Integer>();
         Set<Integer> res2 = set2 != null ? new HashSet<Integer>(set2) : new HashSet<Integer>();
-        res1.removeAll(set1);
-        res2.removeAll(set2);
-        res1.addAll(set2);
+        res1.removeAll(set2);
+        res2.removeAll(set1);
+        res1.addAll(res2);
 
         return res1;
     }
 
     public static void main(String[] args) {
 
-        Set<Integer> intSet1 = Set.of(1, 3, 5, 12, 45);
-        Set<Integer> intSet2 = Set.of(12, 44, 3, 1, 2, 4);
+        Set<Integer> intSet1 = Set.of(0,1,2,4,5,6,9);
+        Set<Integer> intSet2 = Set.of(0,2,3,4,6,7,9,10);
         Set<Integer> union = union(intSet1, intSet2);
         Set<Integer> intersection = intersection(intSet1, intSet2);
         Set<Integer> difference = difference(intSet1, intSet2);
