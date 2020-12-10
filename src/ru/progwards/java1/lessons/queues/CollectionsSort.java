e35c4b768756ac3a1a544b06213a804a4ae37187
Twist328 <evgen-shevel@yandex.ru>
2020-12-11 01:14:59 +0300

    Tr

--- src/ru/progwards/java1/lessons/queues/CollectionsSort.java
+++ src/ru/progwards/java1/lessons/queues/CollectionsSort.java
@@ -7,15 +7,15 @@ public class CollectionsSort {
     public static void main(String[] args) throws IndexOutOfBoundsException {
 
         ArrayList<Integer> list = new ArrayList<Integer>();
-        System.out.println("\n" + "   " + " " + " ДО СОРТИРОВКИ:");
-        for (Integer e : new Integer[]{50, 600, 400, 10, -257, 15, 5, 0, 6, 7, 8, 9, 10, 15, 12, -13, -14,-500})
+        System.out.println("\n" + "   " + " " + " mySort ДО СОРТИРОВКИ:");
+        for (Integer e : new Integer[]{50, 600, 400, 10, -257, 15, 5, 0, -200, 7, 8, 9, 10, 15, 12, -13, -14, -500})
             list.add(e);
 
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i) + " ");
         }
         mySort(list);
-        System.out.print("\n" +" "+" "+ "ПОСЛЕ СОРТИРОВКИ:" + "\n");
+        System.out.print("\n" + " " + " " + "mySort ПОСЛЕ СОРТИРОВКИ:" + "\n");
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i) + " ");
         }
@@ -23,8 +23,10 @@ public class CollectionsSort {
         minSort(list);
         System.out.println("\n" + "MinList: " + list);
 
-    }
+        collSort(list);
+        System.out.println("\n" + "CollSort: " + list);
 
+    }
 
     public static void mySort(Collection<Integer> data) {  //ПЕРЕДЕЛАТЬ ARRAYSORT В МЕТОД СОРТИРОВКИ КОЛЛЕКЦИЙ
         //НЕ ИСПОЛЬЗУЯ ВСТРОЕННЫЕ МЕТОДЫ СОРТИРОВКИ СПИСКОВ МНОЖЕСТВ ОЧЕРЕДЕЙ И КОЛЛЕКЦИЙ;
@@ -58,20 +60,25 @@ public class CollectionsSort {
 
     public static void minSort(Collection<Integer> data) {
 
-        ArrayList<Integer> list;
-        list = (ArrayList<Integer>) data;
+        ArrayList<Integer> list = new ArrayList<Integer>(data);
         TreeSet<Integer> set = new TreeSet<Integer>();
 
-        if (list.size() > 0) {
+        if (data.size() > 0) {
             Integer minList = Collections.min(list);
             set.add(minList);
             list.remove(minList);
-
         }
-        data.clear();
+        //data.clear();
         while (list.isEmpty()) {
-            data.addAll(set);
+            list.addAll(set);
         }
+    }
+
+    public static void collSort(Collection<Integer> data) {
+
+        LinkedList<Integer> intnumbers = new LinkedList<Integer>(data);
+        Collections.sort(intnumbers);
+        System.out.println("КОЛЛЕКЦИЯ ПОСЛЕ ОКОНЧАНИЯ ПРОГРАММЫ" + " " + "Collection.sort() :  " + intnumbers);
     }
 }
 
