13863a4d392bb24cfc64ce00d23b6e5c2da9322d
Twist328 <evgen-shevel@yandex.ru>
2020-12-11 00:13:26 +0300

    Tr

--- src/ru/progwards/java1/lessons/queues/CollectionsSort.java
+++ src/ru/progwards/java1/lessons/queues/CollectionsSort.java
@@ -7,19 +7,24 @@ public class CollectionsSort {
     public static void main(String[] args) throws IndexOutOfBoundsException {
 
         ArrayList<Integer> list = new ArrayList<Integer>();
-        System.out.println("\n" + "   " + " " + " " + " " + "ДО СОРТИРОВКИ");
-        for (Integer e : new Integer[]{50, -500, 600, 400, 10, -257, 15, 5, 0, 6, 7, 8, 9, 10, 15, 12, -13, 14})
+        System.out.println("\n" + "   " + " " + " ДО СОРТИРОВКИ:");
+        for (Integer e : new Integer[]{50, 600, 400, 10, -257, 15, 5, 0, 6, 7, 8, 9, 10, 15, 12, -13, -14,-500})
             list.add(e);
 
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i) + " ");
         }
         mySort(list);
-        System.out.println("\n" + "   " + " " + " " + " " + " " + "ПОСЛЕ СОРТИРОВКИ");
+        System.out.print("\n" +" "+" "+ "ПОСЛЕ СОРТИРОВКИ:" + "\n");
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i) + " ");
         }
+
+        minSort(list);
+        System.out.println("\n" + "MinList: " + list);
+
     }
+
 
     public static void mySort(Collection<Integer> data) {  //ПЕРЕДЕЛАТЬ ARRAYSORT В МЕТОД СОРТИРОВКИ КОЛЛЕКЦИЙ
         //НЕ ИСПОЛЬЗУЯ ВСТРОЕННЫЕ МЕТОДЫ СОРТИРОВКИ СПИСКОВ МНОЖЕСТВ ОЧЕРЕДЕЙ И КОЛЛЕКЦИЙ;
@@ -50,7 +55,29 @@ public class CollectionsSort {
         }
         //Collections.reverse(list);//встроенный метод использовать нельзя..
     }
+
+    public static void minSort(Collection<Integer> data) {
+
+        ArrayList<Integer> list;
+        list = (ArrayList<Integer>) data;
+        TreeSet<Integer> set = new TreeSet<Integer>();
+
+        if (list.size() > 0) {
+            Integer minList = Collections.min(list);
+            set.add(minList);
+            list.remove(minList);
+
+        }
+        data.clear();
+        while (list.isEmpty()) {
+            data.addAll(set);
+        }
+    }
 }
+
+
+
+
 
 
 
