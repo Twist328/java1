9dc11876659d71e1c8b2f18e5c340d27a5e1b441
Twist328 <evgen-shevel@yandex.ru>
2020-12-19 14:46:58 +0300

    юю

--- src/ru/progwards/java1/lessons/maps/SalesInfo.java
+++ src/ru/progwards/java1/lessons/maps/SalesInfo.java
@@ -8,111 +8,63 @@ import java.util.Scanner;
 import java.util.TreeMap;
 
 public class SalesInfo {
+
     private static Map<String, Double> GOODS;
     private static Map<String, AbstractMap.SimpleEntry<Double, Integer>> CONSUMERS;
-    // загрузить заказы из CSV файла: String, String, int, double
 
-    public static int loadOrders(String fileName) {
+    public static int loadOrders(String fileName) {     // загрузить заказы из CSV файла: String, String, int, double
 
         GOODS = new TreeMap<String, Double>();
-
         CONSUMERS = new TreeMap<String, AbstractMap.SimpleEntry<Double, Integer>>();
-
-        int result = 0;
-
-        try (FileReader r = new FileReader(fileName);
-             Scanner s = new Scanner(r)) {
-
-            while (s.hasNext()) {
-
-                result += processLine(s.nextLine());
-
+        int res = 0;
+        try (FileReader reader = new FileReader(fileName);
+             Scanner scan = new Scanner(reader)) {
+            while (scan.hasNext()) {
+                res += processLine(scan.nextLine());
             }
-
         } catch (IOException e) {
-
             throw new RuntimeException(fileName + ":\n" + e.getMessage());
-
         }
-
-        return result;
-
+        return res;
     }
 
-    // загрузить содержимое строки, вернем количество успешно загруженных заказов
-
-    private static int processLine(String str) {
+    private static int processLine(String str) {// загрузить содержимое строки, вернем количество успешно загруженных заказов
 
         String[] words = str.split(",");
-
         if (words.length == 4) {
-
             try {
-
-                String fio = words[0].trim();
-
+                String pers = words[0].trim();
                 String good = words[1].trim();
-
-                int cnt = Integer.parseInt(words[2].trim());
-
+                int count = Integer.parseInt(words[2].trim());
                 double sum = Double.parseDouble(words[3].trim());
-
                 //System.out.println(str);
-
-                //В String - наименование товара, в Double - общая сумма продаж по товару
-
-                GOODS.put(good, GOODS.containsKey(good) ? GOODS.get(good) + sum : sum);
-
-                //В String  - ФИ, в Double - сумма всех покупок покупателя, в Integer - количество покупок
-
-                if (CONSUMERS.containsKey(fio)) {
-
-                    AbstractMap.SimpleEntry<Double, Integer> simpleEntry = CONSUMERS.get(fio);
-
+                GOODS.put(good, GOODS.containsKey(good) ? GOODS.get(good) + sum : sum);    //В String - наименование товара, в Double - общая сумма продаж по товару
+                if (CONSUMERS.containsKey(pers)) {   //В String  - ФИ, в Double - сумма всех покупок покупателя, в Integer - количество покупок
+                    AbstractMap.SimpleEntry<Double, Integer> simpleEntry = CONSUMERS.get(pers);
                     sum += simpleEntry.getKey();
-
-                    cnt += simpleEntry.getValue();
-
+                    count += simpleEntry.getValue();
                 }
-
-                CONSUMERS.put(fio, new AbstractMap.SimpleEntry<Double, Integer>(sum, cnt));
-
+                CONSUMERS.put(pers, new AbstractMap.SimpleEntry<Double, Integer>(sum, count));
                 return 1;
-
             } catch (Exception e) {
 
             }
-
         }
-
         return 0;
-
     }
-
-
 
     public static Map<String, Double> getGoods() {
-
         return GOODS;
-
     }
-
-
 
     public static Map<String, AbstractMap.SimpleEntry<Double, Integer>> getCustomers() {
-
         return CONSUMERS;
-
     }
-
-
-
     public static void main(String[] args) {
 
-        loadOrders("..\\java1\\src\\ru\\progwards\\java1\\lessons\\maps\\SalesInfo.java");
+        loadOrders("..\\java1\\src\\ru\\progwards\\java1\\lessons\\maps\\SalesINFO");
 
         System.out.println(getGoods());
-
         System.out.println(getCustomers());
 
     }
