d0deb301925dc057597a3c4e1293485ede3f5303
Twist328 <evgen-shevel@yandex.ru>
2020-12-26 11:51:07 +0300

    uuu

--- src/ru/progwards/java1/lessons/datetime/Insurance.java
+++ src/ru/progwards/java1/lessons/datetime/Insurance.java
@@ -6,14 +6,10 @@ import java.time.format.DateTimeFormatter;
 
 public class Insurance {
 
-    public Insurance() {
-
-    }
-
     static enum FormatStyle {SHORT, LONG, FULL} // стиль формата даты-времени
 
     private ZonedDateTime start; // дата-время начала действия страховки
-    private Duration duration1; // продолжительность действия
+    private Duration duration; // продолжительность действия
     //Duration val;
 
     public Insurance(ZonedDateTime start) {// установить дату-время начала действия страховки
@@ -45,29 +41,29 @@ public class Insurance {
     }
 
     // установить продолжительность действия страховки
-    public void setDuration(Duration duration) {
-        duration1 = duration;
+    public void setDuration(Duration duration1) {
+        duration = duration1;
     }
 
     // установить продолжительность действия страховки, задав дату-время окончания
     public void setDuration(ZonedDateTime stop) {
-        duration1 = Duration.between(start, stop);
+        duration = Duration.between(start, stop);
     }
 
     public void setDuration(String durationStr, FormatStyle style) {
 
         switch (style) {
             case SHORT: //целое число миллисекунд (тип long)
-                duration1 = Duration.ofMillis(Integer.parseInt(durationStr));
+                duration = Duration.ofMillis(Integer.parseInt(durationStr));
                 break;
             case LONG:
-                LocalDateTime date0 = LocalDateTime.parse("0000-01-01T00:00:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-                LocalDateTime date1 = LocalDateTime.parse(durationStr, DateTimeFormatter.ISO_LOCAL_DATE_TIME).plusMonths(1).plusDays(1);
-                duration1 = Duration.between(date0, date1);
+                LocalDateTime dateTime0 = LocalDateTime.parse("0000-01-01T00:00:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+                LocalDateTime dateTime1 = LocalDateTime.parse(durationStr, DateTimeFormatter.ISO_LOCAL_DATE_TIME).plusMonths(1).plusDays(1);
+                duration = Duration.between(dateTime0, dateTime1);
                 break;
             case FULL:
             default:
-                duration1 = Duration.parse(durationStr);
+                duration = Duration.parse(durationStr);
         }
     }
 
@@ -75,14 +71,14 @@ public class Insurance {
     public void setDuration(int months, int days, int hours) {
         ZonedDateTime zdt = start == null ? ZonedDateTime.now() : start;
         zdt = zdt.plusMonths(months).plusDays(days).plusHours(hours);
-        duration1 = Duration.between(start, zdt);
+        duration = Duration.between(start, zdt);
     }
 
     // проверить валидна ли страховка на указанную дату-время
     public boolean checkValid(ZonedDateTime dateTime) {
-        if (duration1 == null) return dateTime.isAfter(start);
-        ZonedDateTime over = start.plusHours(duration1.toHours());
-        return dateTime.isAfter(start) && dateTime.isBefore(over);
+        if (duration == null) return dateTime.isAfter(start);
+        ZonedDateTime over = start.plusHours(duration.toHours());
+        return dateTime.isAfter(start) | dateTime.isBefore(over);
     }
 
     // вернуть строку формата "Insurance "+start+" "+duration
@@ -99,6 +95,7 @@ public class Insurance {
         System.out.println(date0);
         System.out.println(date1);
         System.out.println(val.toString());
-        System.out.println(val);
+
+        System.out.println(new Insurance("2020-11-30T10:00:00",FormatStyle.LONG));
     }
 }
