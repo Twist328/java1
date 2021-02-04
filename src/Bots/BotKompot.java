5a398ec8da3a06bb7c9a8f8b041c79e2aacf25a9
Twist328 <evgen-shevel@yandex.ru>
2021-02-04 21:42:12 +0300

    hhh

--- src/Bots/BotKompot.java
+++ src/Bots/BotKompot.java
@@ -1,16 +1,69 @@
-package testlesson;
-
-/*import java.awt.*;
-import java.util.Scanner;
-
+package Bots;
 
 import org.telegram.telegrambots.ApiContextInitializer;
-import ru.progwards.java1.lessons.queues.Order;
 import ru.progwards.java1.testlesson.ProgwardsTelegramBot;
 
-public class PizzaBotKompot extends ProgwardsTelegramBot {
+class PizzaBotKompot extends ProgwardsTelegramBot {
 
-    static Menu menu;
+    private final String menu = "У нас есть пицца, напитки, десерт";
+
+    @Override
+    public String processMessage(String text) {
+
+        checkTags(text);
+        if (foundCount() == 1) {
+            if (checkLastFound("Привет"))
+                return "Приветствую тебя, о мой повелитель!\n Что желаешь? " + menu;
+            if (checkLastFound("Как сам"))
+                return "О Привет! Пока сам! \n Что желаешь? " + menu;
+            if (checkLastFound("конец"))
+                return "Спасибо за заказ.";
+            if (checkLastFound("дурак"))
+                return "Не надоругаться. Я не волшебник, я только учусь";
+
+            return "Отлично, добавляю в заказ " + getLastFound() + " Желаешь что-то еще?";
+
+        }
+        if (foundCount() > 1)
+            return "Под твой запрос подходит: \n" + extract() + "Выбери что-то одно, и я добавлю это в заказ.";
+
+        return "Я не понял, возможно у нас этого нет, попробуй сказать по другому." + menu;
+
+    }
+
+            public static void main (String[]args){
+                System.out.println("Hello bot!");
+                ApiContextInitializer.init();
+
+
+                PizzaBotKompot bot = new PizzaBotKompot();
+                bot.username = "PizKomBot";
+                bot.token = "1156742283:AAFI-S2YFXccCEwKA7j5pKhup8QfuCr3KDg";
+
+                bot.addTags("Как сам", "Как ты, Братан, Даров, Салам ");
+                bot.addTags("привет", "Привет, привет,здрасьте, здравствуй, добр, день, вечер, утро, хай ");
+                bot.addTags("конец", "конец, все, стоп, нет");
+                bot.addTags("дурак", "дурак, придурок, идиот, тупой, козел ");
+
+                bot.addTags("Но Вам нужно выбрать \n" + bot.menu, "есть, поесть, Дайте, Чего");
+                bot.addTags("Пицца гавайская", "гавайск, пицц, ананас, куриц");
+                bot.addTags("Пицца маргарита", "маргарит, пицц, моцарелла, сыр, кетчуп, помидор");
+                bot.addTags("Пицца пепперони", "пепперони, пицц, салями, колбас, сыр, кетчуп, помидор");
+
+                bot.addTags("Торт тирамису", "десерт, торт, тирамису, маскарпоне, бисквит");
+                bot.addTags("Торт медовик", "десерт, торт, мед, бисквит");
+                bot.addTags("Эклеры", "десерт, эклер, заварной, крем, тесто");
+
+                bot.addTags("Кола", "напит, пить, кола");
+                bot.addTags("Холодный чай", "напит, пить, чай, липтон, лимон");
+                bot.addTags("Сок", "напит, пить, сок, апельсиноый, яблочный, вишневый");
+                bot.addTags("Кофе", "напит, пить, кофе, американо, капучино, эспрессо");
+
+                bot.start();
+            }
+        }
+
+  /*static Menu menu;
     static Order order;
 
     private final String menuStr = "У нас есть роллы, добавки, напитки и десерт";
---
+++ src/Bots/Main.java
@@ -0,0 +1,7 @@
+package Bots;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello bot!");
+    }
+}
