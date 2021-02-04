c8d91ea7269a8115a1333e393df73cb9fa1cd932
Twist <evgen-shevel@yandex.ru>
2021-02-04 14:33:03 +0300

    mm

---
+++ src/ru/progwards/java1/lessons/abstractnum/Pyramid.java
@@ -0,0 +1,28 @@
+package ru.progwards.java1.lessons.abstractnum;
+/*
+Реализовать класс Pyramid, переопределяющий метод
+public Number volume(), который будет возвращать объем пирамиды, с основанием квадрат, и высотой равной стороне
+ квадрата по формуле Segment*Segment*Segment/3;
+*/
+public class Pyramid extends Figure3D {
+    public Pyramid() {
+        super();
+    }
+
+    public Pyramid(Number segment) {
+        super(segment);
+    }
+    @Override
+    public Number volume() {
+        return segment.div(segment.mul(segment.mul(segment.mul(
+                segment, segment.mul(segment, segment)), // segment * segment * segment
+                Number.newNumber(segment.numType == Number.NumType.INT ? "3" : "" + Math.PI)), // * pi
+                Number.newNumber("4")), Number.newNumber("3")); // * 4 / 3
+
+    }
+    @Override
+    public String toString() {
+        return "Шар с радиусом " + segment.toString() + ", объемом " + volume();
+    }
+
+    }
