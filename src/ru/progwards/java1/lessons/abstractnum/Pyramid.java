36525a9d402b3edee865fde8e3fae5425eb8cede
Twist328 <evgen-shevel@yandex.ru>
2021-02-06 19:27:39 +0300

    hhh

--- src/ru/progwards/java1/lessons/abstractnum/Pyramid.java
+++ src/ru/progwards/java1/lessons/abstractnum/Pyramid.java
@@ -9,12 +9,24 @@ public Number volume(), который будет возвращать объе�
  квадрата по формуле Segment*Segment*Segment/3;
 */
 public class Pyramid extends Figure3D {
+    @Override
+    public String toString() {
+        return "Pyramid{" +
+                "segment=" + segment +
+                '}';
+    }
+
     public Pyramid(Number segment) {
         super(segment);
     }
 
     public Number volume(){
-        return segment.mul(segment.mul(segment)).div(super.segment.mul(segment));
+
+        return (segment.mul(segment.mul(segment)).div((toString("3"))));
+    }
+
+    private Number toString(String s) {
+        return null;
     }
 
 }
