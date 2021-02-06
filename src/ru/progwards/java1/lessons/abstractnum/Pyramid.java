1f4b5fc5fc33b041330ab82e6af80af3f2e83a35
Twist328 <evgen-shevel@yandex.ru>
2021-02-06 20:55:47 +0300

    hhh

--- src/ru/progwards/java1/lessons/abstractnum/Pyramid.java
+++ src/ru/progwards/java1/lessons/abstractnum/Pyramid.java
@@ -1,6 +1,8 @@
 package ru.progwards.java1.lessons.abstractnum;
 
 
+import com.fasterxml.jackson.databind.node.IntNode;
+
 /*
 Реализовать класс Pyramid, переопределяющий метод
 public Number volume(), который будет возвращать объем пирамиды, с основанием квадрат, и высотой равной стороне
@@ -21,7 +23,7 @@ public class Pyramid extends Figure3D {
 
     public Number volume(){
 
-        return segment.mul(segment.mul(segment).div(new DoubleNumber(3.0)));
+        return segment.mul(segment.mul(segment).div(new IntNumber(3.0)));
     }
 }
 
