ad7ed0ecdc96f1e11b640ec055c8689efa8219c1
Twist328 <evgen-shevel@yandex.ru>
2021-02-22 20:08:28 +0300

    lll

--- src/Comparables/Employee.java
+++ src/Comparables/Employee.java
@@ -34,31 +34,27 @@ public class Employee implements Comparable {
 
     @Override
     public int compareTo(Object obj) {
-        //давайте будем сортировать объекты Employee по значению поля id от меньшего к большему
-        //будем возвращать отрицательное число, 0 или положительное число по каждому сравнению объектов
-        // здесь мы просто отнимаем значение поля одного объекта от значения поля другого объекта
-        // в результате получим 1 из 3 вариантов описанных выше вариантов
+
         Employee entry = (Employee) obj;
         int result = name.compareTo((entry.name));
-        if(result != 0) {
+        if (result != 0) {
             return result;
         }
-        result = id-((entry.id));
-        if(result != 0) {
-            return (int) result / Math.abs( result );
+            result = id - ((entry.id));//развёрнутый вариант CompareTo хороший вариант при совпадениях name
+            if (result != 0) {
+                return (int) result / Math.abs(result);
+            }
+            return 0;
         }
-        return 0;
-    }
-
 
 
     public static void main(String[] args) {
         Employee[] empArr = new Employee[4];
 // заполняем массив объектов Employee данными
-        empArr[0] = new Employee(10, "Andrew", 21, 10000);
-        empArr[1] = new Employee(20, "Dmitriy", 49, 20000);
-        empArr[2] = new Employee(5, "Viktor", 45, 5000);
-        empArr[3] = new Employee(1, "Andrew", 42, 50000);
+        empArr[0] = new Employee(25, "Генулик", 21, 10000);//CompareTo если имена совпадают
+        empArr[1] = new Employee(20, "Дмитрий", 49, 20000);//то сортирует по id
+        empArr[2] = new Employee(5, "Виктор", 45, 5000);//если правильно переопределен метод
+        empArr[3] = new Employee(26, "Генулик", 22, 10000);
 // сортируем массив с использованием Comparable
         Arrays.sort(empArr);
         System.out.println("Стандартная сортировка для массива объектов Employee:\n"+Arrays.toString(empArr));
