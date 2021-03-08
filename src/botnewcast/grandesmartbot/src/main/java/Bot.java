e8f70791cf8cde5e15e419bf854e39a3d8dd7f19
Twist328 <evgen-shevel@yandex.ru>
2021-03-08 19:53:43 +0300

    ааа

--- src/botnewcast/grandesmartbot/src/main/java/Bot.java
+++ src/botnewcast/grandesmartbot/src/main/java/Bot.java
@@ -9,20 +9,19 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Bot extends TelegramLongPollingBot {
-    public static final String COLA_DRINKS = "Cola drinks";
-    private static final String WHAT_THE_TIME_REQUEST = "What the time current";
-    private static final String WHAT_THE_DATE_REQUEST = "What the datetime current";
-    private static final String WHAT_IS_CAPITAL_OF_USD_REQUEST = "What is the capital of USD current";
-    private static final String ORDER_PIZZA_REQUEST = "Order pizza";
-    public static final String ROLLS_YAPOSH_SET_8 = "Rolls Yaposh set 8";
-    public static final String TH_CHEESE_36_AS_USUAL = "4 th cheese 36 as usual";
-    public static final String KARBONARA_36_AS_USUAL = "Karbonara 36 as usual";
+    private static final String WHAT_THE_DATE_REQUEST = "Какой сегодня день?";
+    public static final String ПРИВЕТ = "Привет";
+    public static final String НУ_И_КАК_ПОГОДА = "Ну и как погода?";
+    private static final String WHAT_THE_TIME_REQUEST= "Который час?";
+    private Update update;
 
     public static void main(String[] args) {
         System.out.println ("Hello bot!");
@@ -40,34 +39,83 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     @Override
+    public String getBotUsername() {
+        return "SmartyKomBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1646881099:AAFbUeLTpwokLtWjJk0xQlFuxhVdzEantPU";
+    }
+
+    @Override
     public void onUpdateReceived(Update update) {
         Pattern pattern = new Pattern ();
         Message message = update.getMessage ();
-
         SendMessage responce = new SendMessage ();
-
         responce.setText ("Привет,   " + message.getFrom ().getFirstName ()
                 + "! Вы мне прислали: " + message.getText ());
-        message.getText ();
-
         responce.setChatId (message.getChatId ());
         responce.setReplyMarkup (getMainMenu ());
+        if (message != null && message.hasText ()) {
+
+            try {
+                execute (getResponceMessage (message));
+            } catch (TelegramApiException | IOException e) {
+                e.printStackTrace ();
+            }
+            switch (message.getText ()) {
+                case "Привет":
+                    sendMsg (message, "Привет!  Я Бот - Погод!");
+                    break;
+                case "Как погода":
+                    sendMsg (message, "Напишите город и я пришлю погоду в нём!");
+                    break;
+                default:
+                    try {
+                        sendMsg (message, Climat.getWeather (message.getText (), pattern));
+                    } catch (IOException e) {
+                        e.printStackTrace ();
+                    }
+            }
+        }
+    }
+
+    public SendMessage sendMsg(Message message, String text) {
+        SendMessage responce = new SendMessage ();
+        responce.enableMarkdown (true);
+
+        responce.setChatId (message.getChatId ().toString ());
+
+        responce.setReplyToMessageId (message.getMessageId ());
+
+        responce.setText (text);
         try {
-            execute (getResponceMessage (message));
+            sendMessage (responce);
+            responce.setChatId (String.valueOf (getMainMenu ()));
+            responce.setReplyMarkup (getMainMenu ());
+
         } catch (TelegramApiException e) {
             e.printStackTrace ();
         }
-
+        return responce;
     }
 
-    private SendMessage getResponceMessage(Message message) {
+    public Bot() {
+        super ();
+    }
+
+    private SendMessage getResponceMessage(Message message) throws IOException {
+        Pattern pattern = new Pattern ();
         switch (message.getText ()) {
             case WHAT_THE_TIME_REQUEST:
                 return getCurrentTimeResponce (message);
-            case ORDER_PIZZA_REQUEST:
-                return getOrderPizza (message);
+            case ПРИВЕТ:
+                return sendMsg (message, "Я Бот-У, показываю Погоду-y! Напиши город!" );
             case WHAT_THE_DATE_REQUEST:
                 return getCurrentDateResponce (message);
+            case НУ_И_КАК_ПОГОДА:
+                return sendMsg (message, " Напиши город!" );
             default:
                 return greetingMessage (message);
         }
@@ -84,15 +132,6 @@ public class Bot extends TelegramLongPollingBot {
         return responce;
     }
 
-    @Override
-    public String getBotUsername() {
-        return "SmartyKomBot";
-    }
-
-    @Override
-    public String getBotToken() {
-        return "1646881099:AAFbUeLTpwokLtWjJk0xQlFuxhVdzEantPU";
-    }
 
     private ReplyKeyboardMarkup getMainMenu() {
 
@@ -105,8 +144,8 @@ public class Bot extends TelegramLongPollingBot {
 
         KeyboardRow row2 = new KeyboardRow ();
 
-        row2.add (WHAT_IS_CAPITAL_OF_USD_REQUEST);
-        row2.add (ORDER_PIZZA_REQUEST);
+        row2.add (ПРИВЕТ);
+        row2.add (НУ_И_КАК_ПОГОДА);
 
         List<KeyboardRow> rows = new ArrayList<> ();
         rows.add (row1);
@@ -117,7 +156,7 @@ public class Bot extends TelegramLongPollingBot {
 
     private SendMessage getCurrentTimeResponce(Message message) {
         SendMessage responce = new SendMessage ();
-        responce.setText (LocalDateTime.now ().format (DateTimeFormatter.ofPattern ("HH:mm")));
+        responce.setText (LocalDateTime.now ().format (DateTimeFormatter.ofPattern ("Сейчас " + "HH:mm ")) + Emoji.SMILING_FACE_WITH_OPEN_MOUTH_AND_COLD_SWEAT);
         responce.setChatId (message.getChatId ());
         responce.setReplyMarkup (getMainMenu ());
         return responce;
@@ -125,24 +164,32 @@ public class Bot extends TelegramLongPollingBot {
 
     private SendMessage getCurrentDateResponce(Message message) {
         SendMessage responce = new SendMessage ();
-        responce.setText (LocalDateTime.now ().format (DateTimeFormatter.ofPattern ("dd-MM-YYYY")));
+        responce.setText (LocalDateTime.now ().format (DateTimeFormatter.ofPattern ("Сегодня   " + "dd-MM-YYYY ")) + Emoji.WINKING_FACE);
         responce.setChatId (message.getChatId ());
         responce.setReplyMarkup (getMainMenu ());
         return responce;
     }
 
+    /* private SendMessage getCurrentWeaterResponce(Message message) throws IOException {
+         Pattern pattern = new Pattern ();
+         SendMessage responce = new SendMessage ();
+         sendMsg (message, Climat.getWeather (message.getText (), pattern));
+         responce.setChatId (message.getChatId ());
+         responce.setReplyMarkup (getMainMenu ());
+         return responce;
+     }*/
     private ReplyKeyboardMarkup creatChoisePizzaMenu() {
         ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup ();
 
         KeyboardRow row1 = new KeyboardRow ();
 
-        row1.add (KARBONARA_36_AS_USUAL);
-        row1.add (TH_CHEESE_36_AS_USUAL);
+        //row1.add (KARBONARA_36_AS_USUAL);
+        //row1.add (TH_CHEESE_36_AS_USUAL);
 
         KeyboardRow row2 = new KeyboardRow ();
 
-        row2.add (ROLLS_YAPOSH_SET_8);
-        row2.add (COLA_DRINKS);
+        //row2.add (ROLLS_YAPOSH_SET_8);
+        //row2.add (COLA_DRINKS);
 
         List<KeyboardRow> rows = new ArrayList<> ();
         rows.add (row1);
@@ -153,7 +200,7 @@ public class Bot extends TelegramLongPollingBot {
 
     private SendMessage getOrderPizza(Message message) {
         SendMessage responce = new SendMessage ();
-        responce.setText ("Please make your chois:)" + Emoji.SMILING_FACE_WITH_OPEN_MOUTH_AND_TIGHTLY_CLOSED_EYES);
+        responce.setText ("Please make your chois:)" + Emoji.WINKING_FACE);
         responce.setReplyMarkup (creatChoisePizzaMenu ());
         responce.setChatId (message.getChatId ());
         return responce;
@@ -161,7 +208,7 @@ public class Bot extends TelegramLongPollingBot {
 }
 
 enum Emoji {
-    // Emoticones group
+
     GRINNING_FACE_WITH_SMILING_EYES ('\uD83D', '\uDE01'),
     FACE_WITH_TEARS_OF_JOY ('\uD83D', '\uDE02'),
     SMILING_FACE_WITH_OPEN_MOUTH ('\uD83D', '\uDE03'),
