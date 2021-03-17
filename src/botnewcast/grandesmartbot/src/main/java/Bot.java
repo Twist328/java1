605cdc728778c8c0f51436c9d4412414bbc261bf
Twist328 <evgen-shevel@yandex.ru>
2021-03-17 19:48:37 +0300

    nnn

--- src/botnewcast/grandesmartbot/src/main/java/Bot.java
+++ src/botnewcast/grandesmartbot/src/main/java/Bot.java
@@ -18,9 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import static java.lang.System.*;
-
-
 public class Bot extends TelegramLongPollingBot {
 
     public static final String MOSCOW = "МОСКВА";
@@ -48,7 +45,7 @@ public class Bot extends TelegramLongPollingBot {
     private Update update;
 
     public static void main(String[] args) throws IOException {
-        out.println("Hello bot!");
+        System.out.println("Hello bot!");
 
         ApiContextInitializer.init();
 
@@ -342,5 +339,42 @@ public class Bot extends TelegramLongPollingBot {
         responce.setChatId(message.getChatId());
         return responce;
     }
+    enum Emoji {
+
+        GRINNING_FACE_WITH_SMILING_EYES('\uD83D', '\uDE01'),
+        FACE_WITH_TEARS_OF_JOY('\uD83D', '\uDE02'),
+        SMILING_FACE_WITH_OPEN_MOUTH('\uD83D', '\uDE03'),
+        SMILING_FACE_WITH_OPEN_MOUTH_AND_SMILING_EYES('\uD83D', '\uDE04'),
+        SMILING_FACE_WITH_OPEN_MOUTH_AND_COLD_SWEAT('\uD83D', '\uDE05'),
+        SMILING_FACE_WITH_OPEN_MOUTH_AND_TIGHTLY_CLOSED_EYES('\uD83D', '\uDE06'),
+        WINKING_FACE('\uD83D', '\uDE09');
+
+        Character firstChar;
+        Character secondChar;
+
+        Emoji(Character firstChar, Character secondChar) {
+            this.firstChar = firstChar;
+            this.secondChar = secondChar;
+        }
+
+        Emoji() {
+
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+
+            if (this.firstChar != null) {
+                sb.append(this.firstChar);
+            }
+            if (this.secondChar != null) {
+                sb.append(this.secondChar);
+            }
+
+            return sb.toString();
+        }
+    }
+
 }
 
